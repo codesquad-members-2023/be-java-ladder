70192af78ec25a69d4affd54b18970788d49105f
ngw7617@naver.com <ngw7617@naver.com>
2023-03-06 16:34:44 +0900

    🐛fix(Level1):Random이 0만 생성되는 버그 수정
    - Random의 범위를 0~1로 변경하여 0,1로 생성되도록 수정(기존 0만)

--- src/main/java/kr/codesquad/RandomGenerator.java
+++ src/main/java/kr/codesquad/RandomGenerator.java
@@ -5,6 +5,6 @@ import java.util.Random;
 public class RandomGenerator {
     public static boolean generate() {
         Random random = new Random();
-        return (random.nextInt() == 1) ? true : false;
+        return (random.nextInt(2) == 1) ? true : false;
     }
 }
