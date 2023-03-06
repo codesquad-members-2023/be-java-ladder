9546248cd7940c4cd39b825a668e0f22091394ff
JeonHyoChang <jhc1426@naver.com>
2023-03-06 16:41:00 +0900

    refactor: depth 1단계 변경
    기존 for문을 재귀로 구현
    else 문을 쓰는 경우 return 으로 값을 전달

--- src/main/java/Ladder_game/Basic_function.java
+++ src/main/java/Ladder_game/Basic_function.java
@@ -2,8 +2,14 @@ package Ladder_game;
 
 public class Basic_function {
     public static void main(String[] args) {
+        // 사다리 값 입력
         Ladder ladder = new Ladder();
-        ladder.set_Ladder();
-        ladder.print_Ladder();
+
+        // 사다리 설정
+        ladder.set_Ladder(0);
+
+        // 사다리 출력
+        System.out.println();
+        ladder.print_Ladder(0);
     }
 }
