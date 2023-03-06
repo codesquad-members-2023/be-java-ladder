c7be63867743ca0ad21444ee645b865953b09229
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-06 17:43:27 +0900

    Feat: "OutputView.java 기능 추가"
    최대 사다리 높이의 입력을 요구하는 문구를 출력하는 기능 추가

--- src/main/java/kr/codesquad/OutputView.java
+++ src/main/java/kr/codesquad/OutputView.java
@@ -21,4 +21,8 @@ public class OutputView {
     public void printAskHowManyPeople() {
         globalPrintln("참여할 사람은 몇 명인가요?");
     }
+
+    public void printAskHeightOfLadder() {
+        globalPrintln("최대 사다리 높이는 몇 개인가요?");
+    }
 }
