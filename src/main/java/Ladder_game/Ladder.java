7446209f14bf92ff3b8e8da99d5b18f113458c16
JeonHyoChang <jhc1426@naver.com>
2023-03-06 17:26:08 +0900

    feat: 이름 입력 추가
    이름을 분리해주는 기능과 저장하는 변수 선언

--- src/main/java/Ladder_game/Ladder.java
+++ src/main/java/Ladder_game/Ladder.java
@@ -1,23 +1,31 @@
 package Ladder_game;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Ladder {
     private int num;
     private int height;
     String[][] ladder;
+    List<String> name;
 
     // 초기값 세팅
     public Ladder() {
         Scanner in = new Scanner(System.in);
-        System.out.print("참여할 사람은 몇 명인가요?\n");
-        int num = in.nextInt();
+        System.out.print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)\n");
+        this.name = parsing_Name(in.nextLine());
+        this.num = name.size();
         System.out.print("최대 사다리 높이는 몇 개인가요?\n");
-        int height = in.nextInt();
+        this.height = in.nextInt();
 
-        this.num = num;
-        this.height = height;
         ladder = new String[height][num * 2 - 1];
+    }
+
+    // 이름 분리
+    public List<String> parsing_Name(String str){
+        return Arrays.stream(str.split(",")).collect(Collectors.toList());
     }
 
     // 사다리 설정
@@ -30,11 +38,17 @@ public class Ladder {
     // 사다리 내부 값
     public String ladder_Check(int check) {
         if (check % 2 == 0) return "|";
-        return (int) (Math.random() * 2) == 0 ? " " : "-";
+        return (int) (Math.random() * 2) == 0 ? "     " : "-----";
     }
 
+    // 이름 출력
+    public void print_Name(){
+
+    }
+    
     // 사다리 출력
     public void print_Ladder(int line) {
+        if(line == 0) print_Name();
         for (int j = 0; j < num * 2 - 1; j++) System.out.print(ladder[line][j]);
         System.out.println();
 
