7c21bfe511f93167fa2a5ed763ccf3bec1d67f08
Life is a ninanino <tvxqinak@naver.com>
2023-03-06 17:12:08 +0900

    사다리 게임 구현하기 2단계 완료
    커맨드 + 옵션 + m으로 메서드 로직을 빠르게 수정했다

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -4,42 +4,45 @@ import java.util.Scanner;
 
 public class Main {
     static String[][] resultLine;
+    static Scanner sc = new Scanner(System.in);
 
+    // TODO : naming convention - 완료, depth 1로 줄이기 - 완료, 메서드는 10줄 이내 - 완료, 계산하는 로직 메서드로 빼기 - 완료
     public static void main(String[] args) {
-        printMessage();
-        int line = Integer.parseInt(input()); // 3
-        printMessage2();
-        int count = Integer.parseInt(input()); // 5
-        resultLine = new String[count][line*2-1];
-        // 작대기를 어떻게 랜덤으로 세울 것인가? 비면 0, 체우면 1 - 완료
-        // TODO : 입력받은 수만큼 이차원 배열에 넣기
-        for(int i=0; i<count; i++){
-            for(int j=0; j<line*2-1; j++){
-                if(j % 2 == 0) {
-                    System.out.print(makeLine());
-                }
-                if(j%2==1){
-                    System.out.print(makeBridge());
-                }
-            }
+        printMessageHuman();
+        int human = Integer.parseInt(input()); // 3
+        printMessageHeight();
+        int height = Integer.parseInt(input()); // 5
+        resultLine = new String[height][human*2-1];
+        for(int i=0; i<height; i++){
+            makeContents(human);
             System.out.println();
         }
     }
 
-    /**
-     * TODO : 입력받기, 출력메세지, 입력받은 숫자에 따라 나누기
-     */
-    public static String input(){
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        return input;
+    private static void makeContents(int line) {
+        for(int i = 0; i< line*2-1; i++){
+            validateNumber(i);
+        }
     }
 
-    public static void printMessage(){
+    private static void validateNumber(int i) {
+        if(i % 2 == 0) {
+            System.out.print(makeLine());
+        }
+        if(i % 2==1){
+            System.out.print(makeBridge());
+        }
+    }
+
+    public static String input(){
+        return sc.nextLine();
+    }
+
+    public static void printMessageHuman(){
         System.out.println("참여할 사람은 몇 명인가요?");
     }
 
-    public static void printMessage2(){
+    public static void printMessageHeight(){
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
@@ -47,16 +50,10 @@ public class Main {
         return "|";
     }
 
+    // TODO : else 삭제 - 완료
     public static String makeBridge(){
-        String result = "";
-        // input은 세로로 주어지는 줄
         int ran = (int)(Math.random()*2); // 0이상 2미만 -> 0과 1만 나옴
-        if(ran == 0){
-            result = " ";
-        }else{
-            result = "-";
-        }
-        return result;
+        return ran == 1? "-" : " ";
     }
 
 
