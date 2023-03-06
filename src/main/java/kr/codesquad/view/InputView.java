ce13043616b2aa2dd2482719a62d31ce4d27c31f
saehim <saeilee.vxv@gmail.com>
2023-03-06 17:19:17 +0900

    Refactor (InputView) : 생성자 코드 수정

--- src/main/java/kr/codesquad/view/InputView.java
+++ src/main/java/kr/codesquad/view/InputView.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class InputView {
     private final String GET_PERSON_MESSAGE = "참여할 사람은 몇 명인가요?";
     private final String GET_STEP_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
-    private Scanner sc = new Scanner(System.in);
+    private Scanner sc;
+
+    public InputView() {
+        sc = new Scanner(System.in);
+    }
 
     public int getPersonNum() {
         System.out.println(GET_PERSON_MESSAGE);
