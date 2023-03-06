f7578a1a585d364f19487f91c9b28a37e86d0ea7
lvalentine6 <ysr1227@naver.com>
2023-03-06 17:36:16 +0900

    Feat : 생성한 사다리를 출력하는 기능 구현

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -9,6 +9,18 @@ public class OutputView {
         System.out.println(Message.MAX_MEMBER_HEIGHT.getMessage());
     }
 
+    public void printLadderResult(String[][] ladderResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n");
+        for (int i = 0; i < ladderResult.length; i++) {
+            for (int j = 0; j < ladderResult[0].length; j++) {
+                stringBuilder.append(ladderResult[i][j]);
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.println(stringBuilder);
+    }
+
     enum Message {
         JOIN_MEMBER_MESSAGE("참여할 사람은 몇 명인가요?"),
         MAX_MEMBER_HEIGHT("최대 사다리 높이는 몇 개인가요?");
