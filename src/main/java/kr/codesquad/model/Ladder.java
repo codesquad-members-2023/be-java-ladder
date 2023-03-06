02c37ecd7afcd492653b297cda0b4f405723f898
lvalentine6 <ysr1227@naver.com>
2023-03-06 17:34:42 +0900

    Feat : 입력받은 데이터로 사다리를 생성하는 기능 구현

--- src/main/java/kr/codesquad/model/Ladder.java
+++ src/main/java/kr/codesquad/model/Ladder.java
@@ -1,8 +1,31 @@
 package kr.codesquad.model;
 
-public class Ladder implements Generator{
+import java.util.Random;
+
+public class Ladder implements Generator {
+    Random random;
+
+    public Ladder(Random random) {
+        this.random = random;
+    }
+
     @Override
-    public String[][] makeLadder() {
-        return new String[0][];
+    public String[][] makeLadder(String joinMember, String maxLadderHeight) {
+        String[][] ladder = new String[Integer.parseInt(maxLadderHeight)][
+                Integer.parseInt(joinMember) + Integer.parseInt(joinMember) - 1];
+        for (int i = 0; i < ladder.length; i++) {
+            for (int j = 0; j < ladder[0].length; j++) {
+                if (j % 2 == 0) {
+                    ladder[i][j] = "|";
+                    continue;
+                }
+                if (random.nextBoolean()) {
+                    ladder[i][j] = "-";
+                    continue;
+                }
+                ladder[i][j] = " ";
+            }
+        }
+        return ladder;
     }
 }
