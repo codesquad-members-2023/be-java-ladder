de5b0074310c897d6986dda78f90d757a099504f
ngw7617@naver.com <ngw7617@naver.com>
2023-03-06 17:16:56 +0900

    ✨feat(Level2):음수, 숫자가 아닌 경우에 대한 예외 처리 구현
    - 예외 발생 시 함수를 재귀적으로 실행하도록 구현

--- src/main/java/kr/codesquad/View.java
+++ src/main/java/kr/codesquad/View.java
@@ -3,17 +3,33 @@ package kr.codesquad;
 import java.util.Scanner;
 
 public class View {
+
+    public static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자를 입력해주세요.";
+    public static final String READ_PARTICIPANTS_MESSAGE = "참여할 사람은 몇 명인가요?";
+    public static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+
     public static int readParticipants() {
-        System.out.println("참여할 사람은 몇 명인가요?");
+        System.out.println(READ_PARTICIPANTS_MESSAGE);
         Scanner scanner = new Scanner(System.in);
-        //예외 처리 필요
-        return Integer.parseInt(scanner.nextLine());
+        return readNumber(scanner);
     }
 
     public static int readLadderHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(READ_LADDER_HEIGHT_MESSAGE);
         Scanner scanner = new Scanner(System.in);
-        //예외 처리 필요
-        return Integer.parseInt(scanner.nextLine());
+        return readNumber(scanner);
+    }
+
+    private static int readNumber(Scanner scanner) {
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println(NUMBER_FORMAT_ERROR_MESSAGE);
+            return readNumber(scanner);
+        }
+    }
+
+    public static void printMap(String map) {
+        System.out.println(map);
     }
 }
