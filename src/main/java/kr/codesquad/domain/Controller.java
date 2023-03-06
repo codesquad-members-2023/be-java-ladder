5246a8e1c64de9bdb8fe8989890066b9817eee35
birdieHyun <flaehdan@gmail.com>
2023-03-06 16:52:58 +0900

    Feat : Step1 완성
    Body :

--- src/main/java/kr/codesquad/domain/Controller.java
+++ src/main/java/kr/codesquad/domain/Controller.java
@@ -1,9 +1,26 @@
 package kr.codesquad.domain;
 
+import kr.codesquad.AppConfig;
+import kr.codesquad.domain.laddergenerator.LadderGenerator;
+import kr.codesquad.domain.laddergenerator.StepOneLadder;
+import kr.codesquad.view.InputView;
+import kr.codesquad.view.OutputView;
+
 public class Controller {
 
-    public void startGame(){
+    AppConfig appConfig = new AppConfig();
+    InputView inputView = appConfig.inputView();
+    OutputView outputView = appConfig.outputView();
+    LadderGenerator ladderGenerator = appConfig.ladderGenerator();
 
+    public void startGame(){
+        outputView.askPeopleNumber();
+        int people = inputView.inputInt();
+        outputView.askLadderHeight();
+        int height = inputView.inputInt();
+
+        String[][] ladder = ladderGenerator.ladder(people, height);
+        outputView.printLadder(ladder);
     }
 
 }
