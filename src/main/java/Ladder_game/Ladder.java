a19540a9a922986e9b56cffb2671406cbab95d08
JeonHyoChang <jhc1426@naver.com>
2023-03-06 16:50:39 +0900

    refactor: naming convention 수정
    바로 인식 불가한 변수명 수정
    출력부분 오타 수정

--- src/main/java/Ladder_game/Ladder.java
+++ src/main/java/Ladder_game/Ladder.java
@@ -3,37 +3,41 @@ package Ladder_game;
 import java.util.Scanner;
 
 public class Ladder {
-    private int N;
-    private int M;
+    private int num;
+    private int height;
     String[][] ladder;
 
+    // 초기값 세팅
     public Ladder() {
         Scanner in = new Scanner(System.in);
         System.out.print("참여할 사람은 몇 명인가요?\n");
-        int n = in.nextInt();
+        int num = in.nextInt();
         System.out.print("최대 사다리 높이는 몇 개인가요?\n");
-        int m = in.nextInt();
+        int height = in.nextInt();
 
-        this.N = n;
-        this.M = m;
-        ladder = new String[M][N * 2 - 1];
+        this.num = num;
+        this.height = height;
+        ladder = new String[height][num * 2 - 1];
     }
 
+    // 사다리 설정
     public void set_Ladder(int line) {
-        for (int j = 0; j < N * 2 - 1; j++) ladder[line][j] = ladder_Check(j);
+        for (int j = 0; j < num * 2 - 1; j++) ladder[line][j] = ladder_Check(j);
 
-        if (line + 1 < M) set_Ladder(line + 1);
+        if (line + 1 < height) set_Ladder(line + 1);
     }
 
+    // 사다리 내부 값
     public String ladder_Check(int check) {
         if (check % 2 == 0) return "|";
         return (int) (Math.random() * 2) == 0 ? " " : "-";
     }
 
+    // 사다리 출력
     public void print_Ladder(int line) {
-        for (int j = 0; j < N * 2 - 1; j++) System.out.print(ladder[line][j]);
+        for (int j = 0; j < num * 2 - 1; j++) System.out.print(ladder[line][j]);
         System.out.println();
 
-        if (line + 1 < M) set_Ladder(line + 1);
+        if (line + 1 < height) print_Ladder(line + 1);
     }
 }
