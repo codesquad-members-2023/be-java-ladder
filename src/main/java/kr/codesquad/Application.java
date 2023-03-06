02c8329075128160343331ef7c5eb91c49f25fa8
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-06 17:46:22 +0900

    Feat: "Application.java 생성"
    사다리 게임을 실행하는 Application.java 생성

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -3,5 +3,7 @@ package kr.codesquad;
 public class Application {
     public static void main(String[] args) {
 
+        LadderGameController ladderGameController = new LadderGameController(new InputView(), new OutputView());
+        ladderGameController.LadderGameStart();
     }
 }
