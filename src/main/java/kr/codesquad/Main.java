29663a0c2e3aaba4090c727f9ef5b4661eacc5ab
lvalentine6 <ysr1227@naver.com>
2023-03-06 17:36:32 +0900

    Feat : Main 메서드 구현

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,5 +1,9 @@
 package kr.codesquad;
 
+import java.util.Random;
+import kr.codesquad.controller.LadderController;
+import kr.codesquad.model.Generator;
+import kr.codesquad.model.Ladder;
 import kr.codesquad.view.InputView;
 import kr.codesquad.view.OutputView;
 
@@ -7,6 +11,10 @@ public class Main {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
+        Generator ladder = new Ladder(new Random());
+        LadderController ladderController = new LadderController(inputView, outputView, ladder);
+
+        ladderController.startApplication();
 
     }
 }
