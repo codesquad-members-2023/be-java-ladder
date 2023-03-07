f18300ca32df299537bd2dd2d091aa0fd742992b
JeonHyoChang <jhc1426@naver.com>
2023-03-07 10:20:41 +0900

    ♻️refactor : Basic_function 클래스명 수정

--- src/main/java/Ladder_game/BasicFunction.java
+++ src/main/java/Ladder_game/BasicFunction.java
@@ -2,7 +2,7 @@ package Ladder_game;
 
 import java.util.Scanner;
 
-public class Basic_function {
+public class BasicFunction {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("참여할 사람은 몇 명인가요?\n");
