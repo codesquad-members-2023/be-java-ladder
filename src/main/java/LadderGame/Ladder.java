8989ae59f378ca94c1adae29e8bc266c26069dc2
JeonHyoChang <jhc1426@naver.com>
2023-03-07 17:40:04 +0900

    ♻️refactor: Ladder 입력부분 수정

--- src/main/java/LadderGame/Ladder.java
+++ src/main/java/LadderGame/Ladder.java
@@ -3,20 +3,18 @@ package LadderGame;
 import java.util.Scanner;
 
 public class Ladder {
-    private int numPeople;
-    private int height;
+    private final int numPeople;
+    private final int height;
     String[][] ladder;
 
     // 초기값 세팅
     public Ladder() {
         Scanner in = new Scanner(System.in);
         System.out.print("참여할 사람은 몇 명인가요?\n");
-        int numPeople = in.nextInt();
+        this.numPeople = in.nextInt();
         System.out.print("최대 사다리 높이는 몇 개인가요?\n");
-        int height = in.nextInt();
+        this.height = in.nextInt();
 
-        this.numPeople = numPeople;
-        this.height = height;
         ladder = new String[height][numPeople * 2 - 1];
     }
 
