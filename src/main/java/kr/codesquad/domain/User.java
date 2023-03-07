80ec85f968a7d5eae00323bb44eeadd80bd755a9
saehim <saeilee.vxv@gmail.com>
2023-03-07 16:28:03 +0900

    Feat (user) : 참여자 객체를 위한 클래스 생성
    - 참여자 이름을 저장하고 출력하기 위한 클래스 생성

---
+++ src/main/java/kr/codesquad/domain/User.java
@@ -0,0 +1,13 @@
+package kr.codesquad.domain;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+}
