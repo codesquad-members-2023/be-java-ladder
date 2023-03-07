3862e18e09efe674733dfae83682ede1e418d04e
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-07 09:57:44 +0900

    Feat: "Application.java 코드 추가"
    생성자에 LadderMaker 객체 추가

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -3,7 +3,7 @@ package kr.codesquad;
 public class Application {
     public static void main(String[] args) {
 
-        LadderGameController ladderGameController = new LadderGameController(new InputView(), new OutputView());
+        LadderGameController ladderGameController = new LadderGameController(new InputView(), new OutputView(), new LadderMaker());
         ladderGameController.LadderGameStart();
     }
 }
