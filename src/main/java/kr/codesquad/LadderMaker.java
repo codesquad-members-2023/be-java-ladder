46ac8f10c68079974c310335f59cfa199ccf30ab
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-07 15:40:54 +0900

    사람이 위치한 열에 | 문자 삽입하는 로직 생성

--- src/main/java/kr/codesquad/LadderMaker.java
+++ src/main/java/kr/codesquad/LadderMaker.java
@@ -12,5 +12,25 @@ public class LadderMaker {
             Arrays.fill(ladder[i], " ");
         }
 
+        insertLines();
+
+    }
+
+    private void insertLine() {
+        for (int i = 0; i < ladder.lines; i++) {
+            validationEachRow(i);
+        }
+    }
+
+    private void validationEachRow(int row) {
+        for (int col = 0; col < ladder[0].length; col++) {
+            validationEachCol(row, col);
+        }
+    }
+
+    private void validationEachCol(int row, int col) {
+        if (col % 2== 0) {
+            ladder[row][col] = "|";
+        }
     }
 }
