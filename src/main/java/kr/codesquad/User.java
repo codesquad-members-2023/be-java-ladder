52b97fa5c953390afbde838994685d70aad5c78c
Life is a ninanino <tvxqinak@naver.com>
2023-03-07 20:49:15 +0900

    feat : user 메소드 추가
    객체를 분리하기 위해 name을 가지고 있는 user class를 추가함

---
+++ src/main/java/kr/codesquad/User.java
@@ -0,0 +1,13 @@
+package kr.codesquad;
+
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
