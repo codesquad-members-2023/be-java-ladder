5089cedd4e1cd0f33aebfb894875fdc659aca27f
JeonHyoChang <jhc1426@naver.com>
2023-03-07 17:25:29 +0900

    ♻️refactor: Main 입출력 부분 세분화

--- src/main/java/LadderGame/Main.java
+++ src/main/java/LadderGame/Main.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("참여할 사람은 몇 명인가요?\n");
-        int numPeople = in.nextInt();
-        System.out.print("최대 사다리 높이는 몇 개인가요?\n");
-        int height = in.nextInt();
+        // 사다리 값 입력
+        Ladder ladder = new Ladder();
 
-        Ladder ladder = new Ladder(numPeople, height);
-        ladder.set();
-        ladder.print();
+        // 사다리 설정
+        ladder.set(0);
+
+        // 사다리 출력
+        System.out.println();
+        ladder.print(0);
     }
 }
