f12cc08bb731376bea70e9df0da8fd944596a857
saehim <saeilee.vxv@gmail.com>
2023-03-07 19:53:25 +0900

    Feat (ladder) : ladder 객체 생성
    - Ladder 객체 생성
    - users, step, stepSize, userSize를 멤버로 가짐
    - 생성자로 각 객체 생성
    - 각 객체에 있는 값을 반환하는 메서드 구현

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -3,18 +3,35 @@ package kr.codesquad.domain;
 import java.util.List;
 
 public class Ladder {
-    private LadderMaker ladderMaker;
-    private List<String> users;
+    private int stepSize;
+    private int userSize;
+    private Users users;
+    private List<LadderStep> steps;
 
-    public Ladder() {
-
+    public int getStepSize() {
+        return stepSize;
     }
 
-    public void run() {
-
+    public int getUserSize() {
+        return userSize;
     }
 
+    public List<LadderStep> getSteps() {
+        return steps;
+    }
 
+    public Ladder(int floorNum, List<String> userList) {
+        this.stepSize = floorNum;
+        this.userSize = userList.size();
+        this.users = new Users(userList);
+        this.steps = LadderMaker.generate(userSize, floorNum);
+    }
 
+    public LadderStep getAStep(int i) {
+        return steps.get(i);
+    }
 
+    public String getAUser(int i) {
+        return users.getUserName(i);
+    }
 }
