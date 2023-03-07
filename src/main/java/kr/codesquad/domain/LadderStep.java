d410a942086333bb05afc7a29d7383a7914eafc1
saehim <saeilee.vxv@gmail.com>
2023-03-07 20:36:02 +0900

    Feat (LadderStep) : getter, 생성자 구현
    - 생성자의 경우, 인원수를 매개변수로 받아 랜덤 Boolean 리스트를 멤버 lungs 에 저장하는 역할을 한다.

--- src/main/java/kr/codesquad/domain/LadderStep.java
+++ src/main/java/kr/codesquad/domain/LadderStep.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public class LadderStep {
     private List<Boolean> lungs;
+
+    public List<Boolean> getLung() {
+        return lungs;
+    }
+
+    public LadderStep(int userNum) {
+        this.lungs = RandomBoolean.generate(userNum-1);
+    }
 }
