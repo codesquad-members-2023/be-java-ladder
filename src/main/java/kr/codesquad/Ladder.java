443401ba58b26e4aec52e393f37dcce08369c172
Gwonwoo-Nam <ngw7617@naver.com>
2023-03-07 22:30:19 +0900

    ✨feat(Level3):Ladder의 boolean 이차원배열을 ArrayList와 제네릭 타입으로 변경
    - Level3 구현 요구 사항에 맞춰 이차원 배열 대신 ArrayList 사용
    - 자료 타입 하나 바꾸는데 관련된 많은 코드를 변경해야해서 처음부터 유연한 설계를 구상하는 것의 중요성을 느꼈다.

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -1,30 +1,33 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+
 public class Ladder {
     private final int participant;
     private final int height;
-    private boolean[][] ladderMap;
+    private ArrayList<ArrayList> ladderMap;
 
     public Ladder(int participant, int height) {
         this.participant = participant;
         this.height = height;
-        ladderMap = new boolean[height][participant - 1];
+        ladderMap = new ArrayList<>();
         initLadderMap();
     }
 
     private void initLadderMap() {
         for (int floor = 0; floor < height; floor++) {
+            ladderMap.add(new ArrayList());
             initFloor(floor);
         }
     }
 
     private void initFloor(int floor) {
         for (int section = 0; section < participant - 1; section++) {
-            ladderMap[floor][section] = RandomGenerator.generate();
+            ladderMap.get(floor).add(RandomGenerator.generate());
         }
     }
 
-    public boolean[][] getLadderMap() {
+    public ArrayList<ArrayList> getLadderMap() {
         return ladderMap;
     }
 }
