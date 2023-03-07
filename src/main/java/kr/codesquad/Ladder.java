eed6bc5022d60167d97a684dc5b87be8e43b0895
Gwonwoo-Nam <ngw7617@naver.com>
2023-03-07 23:04:29 +0900

    ♻️refactor(Level3):Ladder 클래스의 불필요한 participant, height 필드 제거
    - Ladder 클래스에서 참가자 수와 height 필드는 ladderMap의 사이즈를 통해 확인할 수 있는 정보이므로 불필요한 필드로 제거
    - 생성 시에는 필요하기 때문에, 생성자에서 map을 초기화하는 initMap으로 바로 넘겨주는 방식으로 수정

--- src/main/java/kr/codesquad/Ladder.java
+++ src/main/java/kr/codesquad/Ladder.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class Ladder {
     private List<String> names;
-    private final int participant;
-    private final int height;
     private ArrayList<ArrayList> ladderMap;
 
     public List<String> getNameList() {
@@ -15,20 +13,18 @@ public class Ladder {
 
     public Ladder(List<String> names, int participant, int height) {
         this.names = names;
-        this.participant = participant;
-        this.height = height;
         ladderMap = new ArrayList<>();
-        initLadderMap();
+        initMap(participant, height);
     }
 
-    private void initLadderMap() {
+    private void initMap(int participant, int height) {
         for (int floor = 0; floor < height; floor++) {
             ladderMap.add(new ArrayList());
-            initFloor(floor);
+            initFloor(floor, participant);
         }
     }
 
-    private void initFloor(int floor) {
+    private void initFloor(int floor, int participant) {
         for (int section = 0; section < participant - 1; section++) {
             ladderMap.get(floor).add(RandomGenerator.generate());
         }
