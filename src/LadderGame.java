f7572e62b8ab6f2b22f2c9d7098d95976177051a
jaea-kim <jaeakim98@gmail.com>
2023-03-08 19:10:26 +0900

    player 객체를 가지는 리스트 생성 및 객체 초기화
    플레이어 객체 생성 중 오류 반환 시 다시 플레이어 초기화 메서드를 실행하여 정상적으로 객체 생성하도록 함

--- src/LadderGame.java
+++ src/LadderGame.java
@@ -1,13 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LadderGame {
     private LadderMap ladderMap;
+    private List<Player> playerList;
 
     public void init() {
+        initPlayers();
         mapOutLadderMap(InputView.getPlayerNum(), InputView.getLadderHeight());
     }
 
     private void mapOutLadderMap(int getPlayerNum, int getLadderHeight) {
         ladderMap = new LadderMap(getPlayerNum, getLadderHeight);
 
+    }
+
+    private void initPlayers() {
+        playerList = new ArrayList<>();
+        String[] playersName = InputView.getPlayersName();
+        for (String playerName : playersName) {
+            createPlayer(playerName);
+        }
+    }
+
+    private void createPlayer(String name) {
+        try {
+            playerList.add(new Player(name));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            initPlayers();
+        }
     }
 
     public void start() {
