9e9cec4266119ba152787e5336b46222126f65bd
ngw7617@naver.com <ngw7617@naver.com>
2023-03-08 12:26:49 +0900

    ✨feat(Level3):View.readNumber에서 소수 입력에 대한 예외처리 구현
    - 정수가 아닌 경우를 isNotInteger 메서드에서 정규 표현식으로 예외 발생시키도록 구현
    - 기존의 isNotPositiveNumber 메서드를 isNotPositive로 이름 변경

--- src/main/java/kr/codesquad/View.java
+++ src/main/java/kr/codesquad/View.java
@@ -9,7 +9,6 @@ public class View {
     private static Scanner scanner = new Scanner(System.in);
     private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 양의 정수를 입력해주세요.";
-    private static final String READ_PARTICIPANTS_MESSAGE = "참여할 사람은 몇 명인가요?";
     private static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String COMMA_SEPERATOR = ",";
 
@@ -27,16 +26,24 @@ public class View {
 
     private static int readNumber(Scanner scanner) {
         try {
-            int number = Integer.parseInt(scanner.nextLine());
-            isNotPositiveNumber(number);
-            return number;
+            String input = scanner.nextLine();
+            isNotInteger(input);
+            isNotPositive(Integer.parseInt(input));
+            return Integer.parseInt(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return readNumber(scanner);
         }
     }
 
-    private static void isNotPositiveNumber(int number) {
+    private static void isNotInteger(String input) {
+        if (!input.matches("^\\d*$")) {
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
+        }
+    }
+
+
+    private static void isNotPositive(int number) {
         if (number <= 0) {
             throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
         }
