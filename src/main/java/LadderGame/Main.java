c25752c8c7442ac81e7946d61ae5101cee144d6d
JeonHyoChang <jhc1426@naver.com>
2023-03-08 11:57:33 +0900

    ♻️refactor: 변수 규칙에 따른 변경(aA->a_a)

--- src/main/java/LadderGame/Main.java
+++ src/main/java/LadderGame/Main.java
@@ -3,13 +3,13 @@ package LadderGame;
 public class Main {
     public static void main(String[] args) {
         // 재귀 함수 시작 변수
-        int recursionNum = 0;
+        int recursion_num = 0;
 
         // 사다리 값 입력
         Ladder ladder = new Ladder();
 
         // 사다리 설정
-        ladder.setLadder(recursionNum);
+        ladder.setLadder(recursion_num);
 
         // 사다리 출력
         System.out.println();
