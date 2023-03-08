a33347c6563d1e47ad01619a5b33f288d071a9fd
ngw7617@naver.com <ngw7617@naver.com>
2023-03-08 11:41:30 +0900

    ♻️refactor(Level3):불필요한 View.readParticipants 메서드 제거
    - 참여자의 이름 리스트만으로 참여자 수를 알 수 있으므로 불필요한 메서드 삭제

--- src/main/java/kr/codesquad/LadderGameController.java
+++ src/main/java/kr/codesquad/LadderGameController.java
@@ -1,14 +1,13 @@
 package kr.codesquad;
 
 import static kr.codesquad.View.readNames;
-import static kr.codesquad.View.readParticipantsNumber;
 import static kr.codesquad.View.readLadderHeight;
 import static kr.codesquad.View.printMap;
 import static kr.codesquad.Renderer.renderMap;
 
 public class LadderGameController {
     public static void run() {
-        Ladder ladder = new Ladder(readNames(), readParticipantsNumber(), readLadderHeight());
+        Ladder ladder = new Ladder(readNames(), readLadderHeight());
 
         String map = renderMap(ladder.getLadderMap(), ladder.getNameList());
 
