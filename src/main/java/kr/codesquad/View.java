f4ac3daf80f45051f17aa1f7e7e70162f59e8344
ngw7617@naver.com <ngw7617@naver.com>
2023-03-08 12:28:33 +0900

    ✨feat(Level3):readNames에 사용자 입력에 대한 이름 예외 처리 로직 구현
    - 이름이 2개보다 적은 경우(값을 입력하지 않는 경우 포함)에 대한 예외 로직
    - 재귀적으로 재입력받도록 구현

--- src/main/java/kr/codesquad/View.java
+++ src/main/java/kr/codesquad/View.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class View {
+    public static final String ILLEGAL_NAME_LIST_MESSAGE = "[ERROR] 2명 이상의 쉼표로 구분한 참여자 이름을 입력해주세요.";
     private static Scanner scanner = new Scanner(System.in);
     private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 양의 정수를 입력해주세요.";
@@ -16,7 +17,19 @@ public class View {
         System.out.println(READ_NAMES_MESSAGE);
 
         String input = scanner.nextLine();
+        try {
+            hasMoreThanTwoNames(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readNames();
+        }
         return Arrays.stream(input.split(COMMA_SEPERATOR)).collect(Collectors.toUnmodifiableList());
+    }
+
+    private static void hasMoreThanTwoNames(String input) {
+        if (input.split(COMMA_SEPERATOR).length < 2) {
+            throw new IllegalArgumentException(ILLEGAL_NAME_LIST_MESSAGE);
+        }
     }
 
     public static int readLadderHeight() {
