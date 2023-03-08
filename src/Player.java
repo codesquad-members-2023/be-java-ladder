e5f5bc83b8c32d6bee431332f439f61a26c176f8
jaea-kim <jaeakim98@gmail.com>
2023-03-08 19:01:22 +0900

    Player class 생성 및 인스턴스 변수 name 범위 설정
    매개변수로 들어온 이름의 길이가 정해진 범위를 벗어날 시 에러 던짐

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    private final int MIN_NAME_LENGTH = 1;
+    private final int MAX_NAME_LENGTH = 5;
+
+    private String name;
+
+    public Player(String name) throws IllegalArgumentException {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("ERROR :: 플레이어 이름이 잘못되었습니다.");
+        }
+        this.name = name;
+    }
+}
