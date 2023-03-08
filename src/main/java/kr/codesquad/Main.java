4cdd7327ff451c4404869b17cd6e9f5ed18bccc5
CiRe <dltpwns6@naver.com>
2023-03-08 10:56:36 +0900

    feat : main문 작성
    사다리 타기 프로그램의 메인문을 작성하였다.

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,23 @@
 package kr.codesquad;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputAdaptor inputAdaptor = new InputAdaptor();
+        MakeLadder makeLadder = new MakeLadder();
+        View view = new View();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("사용자의 수와 라인의 길이를 입력해주세요");
+        int lineLength = Integer.parseInt(sc.next());
+        int userNum = Integer.parseInt(sc.next());
+
+        Point[][] pointMap = inputAdaptor.handle(userNum, lineLength);
+
+        int[][] intMap = makeLadder.process(pointMap);
+
+        view.render(intMap);
+
     }
 }
