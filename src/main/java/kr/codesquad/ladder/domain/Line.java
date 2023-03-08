0ea32e9108dcb11208ce44943a77cf1b9fbe8814
saehim <saeilee.vxv@gmail.com>
2023-03-08 15:32:20 +0900

    Feat (Line) : checkDoubleTrue 함수 구현
    - 다리가 막히는 경우 테스트를 위한 메서드 추가
    - 한 줄에 연속으로 다리가 생성될 경우 예외 발생

--- src/main/java/kr/codesquad/ladder/domain/Line.java
+++ src/main/java/kr/codesquad/ladder/domain/Line.java
@@ -14,9 +14,20 @@ public class Line {
         this.bridges = generateBlocks(userNum);
     }
 
-    private List<Bridge> generateBlocks (int userNum) {
+    private List<Bridge> generateBlocks(int userNum) {
         List<Boolean> booleanList = RandomBoolean.generate(userNum - 1);
+
+        for (int i = 1; i < booleanList.size(); i++) {
+            checkDoubleTrue(booleanList.get(i-1), booleanList.get(i));
+        }
+
         return booleanList.stream().map(Bridge::of).collect(Collectors.toList());
+    }
+
+    private void checkDoubleTrue(boolean b1, boolean b2) {
+        if (b1 && b2) {
+            throw new IllegalArgumentException("[exception] 다리 생성 오류");
+        }
     }
 
     public int size() {
