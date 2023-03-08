02fdc600ec41ae5d29aab7aeff6ffbe243ad5dcc
saehim <saeilee.vxv@gmail.com>
2023-03-08 21:24:58 +0900

    Refactor (InputView) : 예외 메시지 통일성을 위해 태그 삽입
    - 입력 예외 발생시 출력되는 메세지의 통일성을 위해 태그를 달아주었다.

--- src/main/java/kr/codesquad/ladder/view/InputView.java
+++ src/main/java/kr/codesquad/ladder/view/InputView.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private final String GET_PERSON_MESSAGE = "참여할 사람은 몇 명인가요?";
     private final String GET_PERSON_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)";
     private final String GET_STEP_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private final String REG = ",";
@@ -27,16 +26,13 @@ public class InputView {
 
     private String getValidInput() throws IOException {
         String result = bufferedReader.readLine();
+
         if (result.length() > 0) {
             return result;
         }
-        System.out.println("다시 입력하세요.");
-        return getValidInput();
-    }
 
-    public int getPersonNum() throws IOException {
-        System.out.println(GET_PERSON_MESSAGE);
-        return Integer.parseInt(bufferedReader.readLine());
+        System.out.println("[exception] 다시 입력하세요.");
+        return getValidInput();
     }
 
     public int getStepNum() throws IOException {
@@ -47,7 +43,7 @@ public class InputView {
             return stepNum;
         }
 
-        System.out.println("더 큰 숫자를 입력하세요.");
+        System.out.println("[exception] 더 큰 숫자를 입력하세요.");
         return getStepNum();
     }
 
@@ -57,7 +53,7 @@ public class InputView {
         try {
             result = Integer.parseInt(bufferedReader.readLine());
         } catch (NumberFormatException e) {
-            System.out.println("잘못된 숫자 형식입니다.");
+            System.out.println("[exception] 잘못된 숫자 형식입니다.");
             return getValidInt();
         }
 
