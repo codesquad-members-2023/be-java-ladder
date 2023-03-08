362adccb9e99dc8b811073f2a86b7730e0f1490d
JeonHyoChang <jhc1426@naver.com>
2023-03-08 10:12:26 +0900

    ♻️refactor: 입력부분 분리 & for문 단축X
    생성자에 있던 입력부분 분리,
    for문 단축X

--- src/main/java/LadderGame/Ladder.java
+++ src/main/java/LadderGame/Ladder.java
@@ -3,40 +3,56 @@ package LadderGame;
 import java.util.Scanner;
 
 public class Ladder {
-    private final int numPeople;
-    private final int height;
-    String[][] ladder;
+    private int numPeople;
+    private int height;
+    private String[][] ladder;
 
     // 초기값 세팅
     public Ladder() {
+        input();
+        ladder = new String[height][numPeople * 2 - 1];
+    }
+
+    // 입력 부분
+    public void input(){
         Scanner in = new Scanner(System.in);
         System.out.print("참여할 사람은 몇 명인가요?\n");
         this.numPeople = in.nextInt();
         System.out.print("최대 사다리 높이는 몇 개인가요?\n");
         this.height = in.nextInt();
-
-        ladder = new String[height][numPeople * 2 - 1];
     }
 
     // 사다리 설정
     public void set(int line) {
-        for (int j = 0; j < numPeople * 2 - 1; j++) ladder[line][j] = LineCheck(j);
+        for (int j = 0; j < numPeople * 2 - 1; j++) {
+            ladder[line][j] = LineCheck(j);
+        }
 
-        if (line + 1 < height) set(line + 1);
+        if (line + 1 < height) {
+            set(line + 1);
+        }
     }
 
     // 사다리 내부 값
     public String LineCheck(int check) {
-        if (check % 2 == 0) return "|";
-        if ((int) (Math.random() * 2) == 0) return " ";
+        if (check % 2 == 0) {
+            return "|";
+        }
+        if ((int) (Math.random() * 2) == 0) {
+            return " ";
+        }
         return "-";
     }
 
     // 사다리 출력
     public void print(int line) {
-        for (int j = 0; j < numPeople * 2 - 1; j++) System.out.print(ladder[line][j]);
+        for (int j = 0; j < numPeople * 2 - 1; j++) {
+            System.out.print(ladder[line][j]);
+        }
         System.out.println();
 
-        if (line + 1 < height) print(line + 1);
+        if (line + 1 < height) {
+            print(line + 1);
+        }
     }
 }
