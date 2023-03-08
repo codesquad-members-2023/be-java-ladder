db01300dd53fcc584a694129510ea4a6b9ec4a7b
jaea-kim <jaeakim98@gmail.com>
2023-03-08 21:14:36 +0900

    사다리 지도 한 줄을 초기화하는 기능 구현
    point의 위치가 홀수일 경우 바운더리로 판단
    point의 위치가 짝수일 경우 이전 짝수의 상태를 기준으로 판단

--- src/Line.java
+++ src/Line.java
@@ -1,10 +1,50 @@
+import java.util.Random;
+
 public class Line {
     private final char BORDER_LINE = '|';
     private final char HORIZONTAL_LINE = '-';
     private final char EMPTY_LINE = ' ';
+
     private char[] points;
+    private boolean isHorizontalLine = false;
 
     public Line(int lineLength) {
         points = new char[lineLength];
+        init();
+    }
+
+    private void init() {
+        for (int i = 0; i < points.length; i++) {
+            points[i] = getState(i);
+        }
+    }
+
+    private char getState(int point) {
+        if (isBorderCome(point)) { //point의 위치가 짝수일 경우
+            return BORDER_LINE;
+        }
+        return getHorizontalLineState();
+    }
+
+    private char getHorizontalLineState() { //point의 위치가 홀수일 경우
+        if (isHorizontalLine) { //이전 홀수 위치의 가로선 여부 확인 (있으면 : true, 없으면 : false)
+            isHorizontalLine = false;
+            return EMPTY_LINE; //존재할 경우 가로선이 연속으로 올 수 없으므로 빈라인 반환
+        }
+        return selectRandomHorizontalLineState(); //가로선이 올 수 있으므로 랜덤으로 선택
+    }
+
+    private char selectRandomHorizontalLineState() {
+        Random random = new Random();
+
+        if (random.nextBoolean()) {
+            isHorizontalLine = true;
+            return HORIZONTAL_LINE;
+        }
+        return EMPTY_LINE;
+    }
+
+    private boolean isBorderCome(int i) {
+        return i % 2 == 0;
     }
 }
