1bf2c874c9988aec95b34a76d3d58234a3af323e
lvalentine6 <ysr1227@naver.com>
2023-03-08 20:33:11 +0900

    Refactor : print 메서드 상수 적용
    지역변수를 상수처리

--- src/main/java/kr/codesquad/view/View.java
+++ src/main/java/kr/codesquad/view/View.java
@@ -11,12 +11,12 @@ public class View {
     }
 
     public void printJoinMembers() {
-        String JOIN_MEMBER_MESSAGE = "참여할 사람은 몇 명인가요?";
+        final String JOIN_MEMBER_MESSAGE = "참여할 사람은 몇 명인가요?";
         System.out.println(JOIN_MEMBER_MESSAGE);
     }
 
     public void printMaxLadderHeight() {
-        String MAX_MEMBER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+        final String MAX_MEMBER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
         System.out.println(MAX_MEMBER_HEIGHT_MESSAGE);
     }
 
