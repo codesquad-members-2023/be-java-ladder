2387c1cf7866b411eb8c9d6203a826ca1109bebc
saehim <saeilee.vxv@gmail.com>
2023-03-08 17:23:07 +0900

    Test (User) : 이름 중복시 기능 수행
    - 중복 이름이 발생한경우, 해당 이름은 제외하고 게임을 생성하는 기능에 대한 테스트

--- src/test/java/kr/codesquad/ladder/domain/UserTest.java
+++ src/test/java/kr/codesquad/ladder/domain/UserTest.java
@@ -13,11 +13,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
     // pobi,honux,crong,jk
     Users users;
+    String given;
 
     @Test @Order(1)
     @DisplayName("이름을 삽입했을 때, user가 해당 숫자만큼 생성")
     void init() {
-        String given = "pobi,honux,crong,jk";
+        given = "pobi,honux,crong,jk";
         List<String> givenNames = List.of(given.split(","));
         users = new Users(givenNames);
 
@@ -28,10 +29,29 @@ class UserTest {
     @DisplayName("이름이 5글자를 넘어갈 때 IllegalArgumentException 발생")
     public void whenOverLength() throws Exception{
         //given
-        String given = "pobi,honux,crong,jjjkkkkkk";
+        given = "pobi,honux,crong,jjjkkkkkk";
         List<String> givenNames = List.of(given.split(","));
 
         assertThatThrownBy(() -> new Users(givenNames))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test @Order(3)
+    @DisplayName("중복 이름 발생한 경우, 해당 이름은 제외")
+    public void whenBlank() throws Exception{
+        //given
+        given = "pobi,honux,crong,jk,crong";
+        List<String> givenNames = List.of(given.split(","));
+
+        //when
+        Users users = new Users(givenNames);
+
+        //then
+        assertThat(users.size()).isEqualTo(4);
+    }
+
+    // 테스트를 위한 간이 Input
+    private List<String> makeGivenNames (String name) {
+
     }
 }
