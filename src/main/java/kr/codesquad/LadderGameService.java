8c44190273ed8c94fa1f36701c8de0c2f323b899
ngw7617@naver.com <ngw7617@naver.com>
2023-03-08 11:13:41 +0900

    ♻️refactor(Level3):Renderer static 메소드로 변경 및 Controller에서 static import로 가져오도록 변경
    - Renderer는 한 개의 객체만 생성되므로 static으로 변경 및 static import로 사용하도록 수정
    - 난수 생성 시 random.nextInt -> random.nextBoolean

--- src/main/java/kr/codesquad/LadderGameService.java
+++
@@ -1,4 +0,0 @@
-package kr.codesquad;
-
-public class LadderGameService {
-}
--- src/main/java/kr/codesquad/RandomGenerator.java
+++ src/main/java/kr/codesquad/RandomGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomGenerator {
     public static boolean generate() {
         Random random = new Random();
-        if (random.nextInt(2) == 1) {
+        if (random.nextBoolean()) {
             return true;
         }
         return false;
