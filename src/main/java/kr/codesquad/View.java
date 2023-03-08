d8a6e902bef981f562e2bfd8a931d4ca6ffd483e
ngw7617@naver.com <ngw7617@naver.com>
2023-03-08 11:34:08 +0900

    ♻️refactor(Level3):View의 메시지 접근 제어자 public → private 일괄 변경
    - View의 메시지는 클래스 내에서만 접근하므로 private 접근 제어자로 변경
    - comma 매직넘버 constant로 리팩터링

--- src/main/java/kr/codesquad/View.java
+++ src/main/java/kr/codesquad/View.java
@@ -6,28 +6,26 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class View {
-    public static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-
-    public static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 양의 정수를 입력해주세요.";
-    public static final String READ_PARTICIPANTS_MESSAGE = "참여할 사람은 몇 명인가요?";
-    public static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static Scanner scanner = new Scanner(System.in);
+    private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 양의 정수를 입력해주세요.";
+    private static final String READ_PARTICIPANTS_MESSAGE = "참여할 사람은 몇 명인가요?";
+    private static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String COMMA_SEPERATOR = ",";
 
     public static List<String> readNames() {
         System.out.println(READ_NAMES_MESSAGE);
-        Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        return Arrays.stream(input.split(",")).collect(Collectors.toUnmodifiableList());
+        return Arrays.stream(input.split(COMMA_SEPERATOR)).collect(Collectors.toUnmodifiableList());
     }
     public static int readParticipantsNumber() {
         System.out.println(READ_PARTICIPANTS_MESSAGE);
-        Scanner scanner = new Scanner(System.in);
         return readNumber(scanner);
     }
 
     public static int readLadderHeight() {
         System.out.println(READ_LADDER_HEIGHT_MESSAGE);
-        Scanner scanner = new Scanner(System.in);
         return readNumber(scanner);
     }
 
