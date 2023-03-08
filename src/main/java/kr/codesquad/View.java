c759d5df6a7e406e61fce70dbbc6c92f23849d65
ngw7617@naver.com <ngw7617@naver.com>
2023-03-08 14:20:41 +0900

    ✨feat(Level3):사용자 이름 최대 글자 수 초과 예외 처리 구현
    - 최대 5글자를 초과하는 경우에 대해 예외 처리 구현

--- src/main/java/kr/codesquad/View.java
+++ src/main/java/kr/codesquad/View.java
@@ -3,11 +3,14 @@ package kr.codesquad;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class View {
-    public static final String ILLEGAL_NAME_LIST_MESSAGE = "[ERROR] 2명 이상의 쉼표로 구분한 참여자 이름을 입력해주세요.";
     private static Scanner scanner = new Scanner(System.in);
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String ILLEGAL_NAME_LIST_MESSAGE = "[ERROR] 2명 이상의 쉼표로 구분한 참여자 이름을 입력해주세요.";
+    private static final String EXCEED_NAME_LENGTH_MESSAGE = "[ERROR] 이름은 최대 5글자입니다.";
     private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 양의 정수를 입력해주세요.";
     private static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
@@ -19,11 +22,21 @@ public class View {
         String input = scanner.nextLine();
         try {
             hasMoreThanTwoNames(input);
+            exceedMaxNameLength(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return readNames();
         }
         return Arrays.stream(input.split(COMMA_SEPERATOR)).collect(Collectors.toUnmodifiableList());
+    }
+
+    private static void exceedMaxNameLength(String input) {
+        Consumer nameLengthExceptionHandler = o -> {
+            throw new IllegalArgumentException(EXCEED_NAME_LENGTH_MESSAGE);
+        };
+        Arrays.stream(input.split(COMMA_SEPERATOR))
+                .filter(e -> e.length() > MAX_NAME_LENGTH)
+                .forEach(nameLengthExceptionHandler);
     }
 
     private static void hasMoreThanTwoNames(String input) {
@@ -54,7 +67,6 @@ public class View {
             throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
         }
     }
-
 
     private static void isNotPositive(int number) {
         if (number <= 0) {
