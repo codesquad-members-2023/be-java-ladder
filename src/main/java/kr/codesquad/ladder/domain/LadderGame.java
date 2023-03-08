41ee736f4e54cbeb8314908ae8142897d155f3f5
saehim <saeilee.vxv@gmail.com>
2023-03-08 18:53:05 +0900

    Refactor (LadderGame) : InputView, OutputView, Ladder 객체를 멤버 변수에 추가
    - 기존 지역변수에 있었던 변수를 필드로 뺐다.
    - LadderGame이 어떤 객체에 의존하는지를 표현하려고 해봤는데.. 사실 맞는 방향인지 헷갈린다.

--- src/main/java/kr/codesquad/ladder/domain/LadderGame.java
+++ src/main/java/kr/codesquad/ladder/domain/LadderGame.java
@@ -7,17 +7,24 @@ import java.io.IOException;
 import java.util.List;
 
 public class LadderGame {
+    private Ladder ladder;
+    private InputView inputView;
+    private OutputView outputView;
+
+    public LadderGame() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
     public void run() {
         try {
-            InputView inputView = new InputView();
-            OutputView outputView = new OutputView();
-
             List<String> userNames = inputView.getUserNames();
             int stepNum = inputView.getStepNum();
 
-            Ladder ladder = new Ladder(stepNum, userNames);
+            ladder = new Ladder(stepNum, userNames);
 
             System.out.println(outputView.getStringLadder(ladder));
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } catch (IllegalArgumentException e) {
