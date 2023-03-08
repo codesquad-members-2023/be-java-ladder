f5c1f3df9d5d2f9bda89bbb3d7ff81584b9d2758
saehim <saeilee.vxv@gmail.com>
2023-03-08 20:44:22 +0900

    Refactor (bridge) : 멤버 form 추가하여 다리 모양을 출력 메서드를 간결하게 개선
    - 변수를 줄이고 출력시 코드를 간결하게 하기 위해 `Bridge` enum에 `String form` 필드를 만들었다.
    - `toString`의 반환값을 form을 그대로 출력하도록 하여 코드를 더욱 간결하게 했다.

--- src/main/java/kr/codesquad/ladder/domain/Bridge.java
+++ src/main/java/kr/codesquad/ladder/domain/Bridge.java
@@ -1,12 +1,23 @@
 package kr.codesquad.ladder.domain;
 
 public enum Bridge {
-    BRIDGE, EMPTY;
+    BRIDGE("------"), EMPTY("      ");
+
+    private String form;
+
+    Bridge(String form) {
+        this.form = form;
+    }
 
     public static Bridge of(boolean plan) {
         if (plan) {
             return BRIDGE;
         }
         return EMPTY;
+    }
+
+    @Override
+    public String toString() {
+        return form;
     }
 }
--- src/main/java/kr/codesquad/ladder/view/OutputView.java
+++ src/main/java/kr/codesquad/ladder/view/OutputView.java
@@ -6,8 +6,6 @@ import kr.codesquad.ladder.domain.Line;
 public class OutputView {
 
     private final String LADDER_BAR = "|";
-    private final String LADDER_PATH = "------";
-    private final String LADDER_BLOCK = "      ";
     private final String NEW_LINE = System.lineSeparator();
     private StringBuilder stringBuilder;
 
@@ -16,30 +14,31 @@ public class OutputView {
     }
 
     public String getStringLadder(Ladder ladder) {
-        stringBuilder.append(ladder.getAUser(0));
-        for (int i=1; i<ladder.getNumUser(); i++) {
-            stringBuilder.append(String.format("%6s", ladder.getAUser(i)));
-        }
+        printUserNames(ladder);
         stringBuilder.append(NEW_LINE);
+        printLadderForm(ladder);
+        return stringBuilder.toString();
+    }
 
-        for (int i=0; i<ladder.getNumStep(); i++) {
+    private void printLadderForm(Ladder ladder) {
+        for (int i = 0; i< ladder.getNumStep(); i++) {
             stringBuilder.append(LADDER_BAR);
             getStringLadderALine(ladder.getAStep(i));
             stringBuilder.append(NEW_LINE);
         }
-        return stringBuilder.toString();
+    }
+
+    private void printUserNames(Ladder ladder) {
+        stringBuilder.append(ladder.getAUser(0));
+
+        for (int i = 1; i< ladder.getNumUser(); i++) {
+            stringBuilder.append(String.format("%6s", ladder.getAUser(i)));
+        }
     }
 
     private void getStringLadderALine(Line step) {
         for (int j = 0; j< step.size(); j++) {
-            stringBuilder.append(getPath(step.getValidBridge(j))).append(LADDER_BAR);
+            stringBuilder.append(step.getValidBridge(j)).append(LADDER_BAR);
         }
-    }
-
-    private String getPath(boolean lung) {
-        if (lung) {
-            return LADDER_PATH;
-        }
-        return LADDER_BLOCK;
     }
 }
