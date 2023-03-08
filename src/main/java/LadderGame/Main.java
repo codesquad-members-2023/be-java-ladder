50570a3a6b99f0abb2bc31ab05db0d069be00eec
JeonHyoChang <jhc1426@naver.com>
2023-03-08 10:08:13 +0900

    ♻️refactor: 매직넘버 제거
    의미 없는 매직넘버 제거후 의미 부여

--- src/main/java/LadderGame/Main.java
+++ src/main/java/LadderGame/Main.java
@@ -2,14 +2,17 @@ package LadderGame;
 
 public class Main {
     public static void main(String[] args) {
+        // 재귀 함수 시작 변수
+        int recursionNum = 0;
+
         // 사다리 값 입력
         Ladder ladder = new Ladder();
 
         // 사다리 설정
-        ladder.set(0);
+        ladder.set(recursionNum);
 
         // 사다리 출력
         System.out.println();
-        ladder.print(0);
+        ladder.print(recursionNum);
     }
 }
