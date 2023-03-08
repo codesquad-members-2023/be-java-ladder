669ca096fff2fbce528211262fd16d74de290bb7
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-08 20:12:15 +0900

    연속된 다리는 생성되지 않는 기능 구현
    난수 시퀀스에서 prev과 현재가 같다면 현재 다리는 생성되지 않도록 함
    indent를 맞추기 위해 함수를 만들었지만
    매개변수를 3개나 사용해서 줄일 수 있는 방법을 고민해야겠다.

--- src/main/java/kr/codesquad/RandomsGenerator.java
+++ src/main/java/kr/codesquad/RandomsGenerator.java
@@ -14,6 +14,23 @@ public class RandomsGenerator {
             randoms[i] = random;
         }
 
+        return validationContinuous(randoms);
+    }
+
+    private int[] validationContinuous(int[] randoms) {
+        int prev = randoms[0];
+
+        for (int i = 1; i < randoms.length; i++) {
+            continuesBridge(randoms, prev, i);
+            prev = randoms[i];
+        }
+
         return randoms;
+    }
+
+    private void continuesBridge(int[] randoms, int prev, int i) {
+        if (prev % 2 == 0 && randoms[i] % 2 == 0) {
+            randoms[i] = 1;
+        }
     }
 }
