aa54a4a57351fb34e29bcc12f27e5f56daa08407
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-08 12:45:35 +0900

    Refactor: LadderMaker.java 코드 수정
    10줄이 넘어가는 메서드 분리 후 10줄 이내로 수정

--- src/main/java/kr/codesquad/LadderMaker.java
+++ src/main/java/kr/codesquad/LadderMaker.java
@@ -10,25 +10,27 @@ public class LadderMaker {
 
     public String[][] createLadder(int people, int height) {
         ladder = new String[height][people + (people - 1)];
-
         for (int i = 0; i < ladder.length; i++) {
             fillLadderRow(i);
         }
-
         return ladder;
     }
 
     public void fillLadderRow(int row) {
         checkConnected = false;
         for (int i = 0; i < ladder[row].length; i++) {
-            if (checkConnected && i > connectedIndex + 2) {
-                checkConnected = false;
-            }
+            initializeCheckConnected(i, connectedIndex);
             if (i % 2 == 0) {
                 ladder[row][i] = "|";
                 continue;
             }
             ladder[row][i] = decideWhetherConnected(i);
+        }
+    }
+
+    private void initializeCheckConnected(int index, int connectedIndex) {
+        if (checkConnected && index > connectedIndex + 2) {
+            checkConnected = false;
         }
     }
 
