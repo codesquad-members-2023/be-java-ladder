e44d154fe99ee121053da710563f130698d55ba3
Hyun <ghkdgus29@naver.com>
2023-03-08 15:56:37 +0900

    fix: UI 클래스 주석처리
    UI 클래스와 UI 테스트는 추후에 View 클래스를 만들어 전부 수정할 것이기 때문에 전부 주석처리하여 컴파일 에러 제거

--- src/main/java/kr/codesquad/view/UI.java
+++ src/main/java/kr/codesquad/view/UI.java
@@ -6,32 +6,32 @@ import java.util.List;
 
 public class UI {
 
-    private final Ladder ladder;
-
-    public UI(Ladder ladder) {
-        this.ladder = ladder;
-    }
-
-    public void printName() {
-        List<String> people = ladder.getPeople();
-        people.forEach(name -> System.out.printf("%-6s", name));
-        System.out.println();
-    }
-
-    public void drawBridge() {
-        List<List<Boolean>> ladderMap = ladder.getLadder();
-        for (int y = 0; y < ladderMap.size(); y++) {
-            System.out.print("  |");
-            drawLine(ladderMap, y);
-            System.out.println();
-        }
-    }
-
-    private static void drawLine(List<List<Boolean>> ladderMap, int y) {
-        for (int x = 0; x < ladderMap.get(0).size(); x++) {
-            System.out.print(ladderMap.get(y).get(x) ? "-----|" : "     |");
-        }
-    }
+//    private final Ladder ladder;
+//
+//    public UI(Ladder ladder) {
+//        this.ladder = ladder;
+//    }
+//
+//    public void printName() {
+//        List<String> people = ladder.getPeople();
+//        people.forEach(name -> System.out.printf("%-6s", name));
+//        System.out.println();
+//    }
+//
+//    public void drawBridge() {
+//        List<List<Boolean>> ladderMap = ladder.getLadder();
+//        for (int y = 0; y < ladderMap.size(); y++) {
+//            System.out.print("  |");
+//            drawLine(ladderMap, y);
+//            System.out.println();
+//        }
+//    }
+//
+//    private static void drawLine(List<List<Boolean>> ladderMap, int y) {
+//        for (int x = 0; x < ladderMap.get(0).size(); x++) {
+//            System.out.print(ladderMap.get(y).get(x) ? "-----|" : "     |");
+//        }
+//    }
 
 
 }
