b418eae35ab85fa9d4f85ebb06d82f41e739939f
Hyun <ghkdgus29@naver.com>
2023-03-08 15:18:13 +0900

    feat: 사다리 한 줄에 해당하는 Line 클래스 생성
    Line 클래스는 생성자에서 RandomGenerator 를 사용하여 랜덤 Boolean 리스트를 생성한다.
    이때 랜덤 Boolean 리스트가 true 가 연속으로 발생하는 경우, 불가능한 사다리 한 줄이라 판단하여 다시 랜덤 Boolean 리스트를 생성한다.

---
+++ src/main/java/kr/codesquad/domain/Line.java
@@ -0,0 +1,25 @@
+package kr.codesquad.domain;
+
+import java.util.List;
+
+public class Line {
+
+    private final RandomGenerator randomGenerator = new RandomGenerator();
+    private final List<Boolean> points;
+
+    public Line(List<String> people) {
+        List<Boolean> randomBooleans = randomGenerator.generate(people.size() - 1);
+        while (isOverlapping(randomBooleans)) {
+            randomBooleans = randomGenerator.generate(people.size() - 1);
+        }
+
+        points = randomBooleans;
+    }
+
+    public boolean isOverlapping(List<Boolean> randomBooleans) {
+        for (int i = 1; i < randomBooleans.size(); i++) {
+            if (randomBooleans.get(i) && randomBooleans.get(i - 1)) return true;
+        }
+        return false;
+    }
+}
