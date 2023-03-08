f0e30436a2941c3c6104d11183a6865517bc9c94
kimdongmin <kimdongmin@kimdongminui-MacBookPro.local>
2023-03-08 22:29:54 +0900

    OutView에서 OutputView로 클래스명 변경

--- src/main/java/kr/codesquad/OutputView.java
+++ src/main/java/kr/codesquad/OutputView.java
@@ -1,6 +1,6 @@
 package kr.codesquad;
 
-public class OutView {
+public class OutputView {
 
     private static final String HOW_MANY_PARTICIPATE = "참여할 사람은 몇 명인가요?";
     private static final String HOW_MUCH_HIGH = "최대 사다리 높이는 몇 개인가요?";
@@ -13,17 +13,17 @@ public class OutView {
         System.out.println(HOW_MUCH_HIGH);
     }
 
-    public static void printWholeLadder(Point[][] ladder) {
+    public static void printLadder(Shape[][] ladder) {
         StringBuilder sb = new StringBuilder();
-        for (Point[] row : ladder) {
+        for (Shape[] row : ladder) {
             printLadderRow(row, sb);
             sb.append("\n");
         }
         System.out.println(new String(sb));
     }
 
-    private static StringBuilder printLadderRow(Point[] row, StringBuilder sb) {
-        for (Point point : row) {
+    private static StringBuilder printLadderRow(Shape[] row, StringBuilder sb) {
+        for (Shape point : row) {
             sb.append(point);
         }
         return sb;
