079bb4142355f03ccc3631aac1edad34efbb4e91
kimdongmin <kimdongmin@kimdongminui-MacBookPro.local>
2023-03-08 16:26:17 +0900

    <feat> 미션2 리팩토링 수행
    모든 클래스에서 depth를 1로 줄임. 메소드 최대 10라인 이하로 수정.
    리팩토링을 수행하면서 Ladder 클래스를 새롭게 생성하고 사다리 생성 기능을 부여함. Ladder 클래스는 Point[][] 배열을 가지고, 객체 생성시 배열의 인덱스에 Point 객체를 조건에 맞게 생성.
    
    OutView 클래스에서 만들어진 사다리를 매개변수로 받아 출력하는 기능 구현.

--- src/main/java/kr/codesquad/LadderGame.java
+++ src/main/java/kr/codesquad/LadderGame.java
@@ -1,59 +1,25 @@
 package kr.codesquad;
 
-import java.io.IOException;
-import java.util.Random;
+import static kr.codesquad.OutView.*;
 
 public class LadderGame {
     private InputView inputView;
-    private int participants;
-    private int ladderHeight;
-    private String[][] LADDER;
+    private Ladder ladder;
 
     public LadderGame() {
         inputView = new InputView();
     }
 
     public void start() {
-        OutView.printHowManyParticipates();
-        participants = inputView.getParticipantsNumber();
+        printHowManyParticipates();
+        int participantsNum = inputView.getParticipantsNumber();
 
-        OutView.printHowMuchHigh();
-        ladderHeight = inputView.getLadderHeight();
+        printHowMuchHigh();
+        int ladderheight = inputView.getLadderHeight();
 
-        makeLadder();
-        printLadder();
+        ladder = new Ladder(participantsNum, ladderheight);
+        ladder.makeLadder();
+        printWholeLadder(ladder.getLadder());
         System.out.println();
-    }
-
-    // 전달 받은 인원수와 사다리 높이를 기준으로 2차원 배열 만들기
-    private void makeLadder() {
-        LADDER = new String[ladderHeight][participants + participants - 1];
-        for (int i = 0; i < LADDER.length; i++) {
-            for (int j = 0; j < LADDER[i].length; j++) {
-                if (j % 2 == 0) {
-                    LADDER[i][j] = "|";
-                    continue;
-                }
-                drawLine(i, j);
-            }
-        }
-    }
-
-    private void printLadder() {
-        for (String[] line : LADDER) {
-            for (String area : line) {
-                System.out.print(area);
-            }
-            System.out.println();
-        }
-    }
-
-    private void drawLine(int x, int y) {
-        Random random = new Random();
-        if (random.nextBoolean()) {
-            LADDER[x][y] = "-";
-        } else {
-            LADDER[x][y] = " ";
-        }
     }
 }
--- src/main/java/kr/codesquad/OutView.java
+++ src/main/java/kr/codesquad/OutView.java
@@ -4,7 +4,6 @@ public class OutView {
 
     private static final String HOW_MANY_PARTICIPATE = "참여할 사람은 몇 명인가요?";
     private static final String HOW_MUCH_HIGH = "최대 사다리 높이는 몇 개인가요?";
-    private StringBuilder sb;
 
     public static void printHowManyParticipates() {
         System.out.println(HOW_MANY_PARTICIPATE);
@@ -14,4 +13,19 @@ public class OutView {
         System.out.println(HOW_MUCH_HIGH);
     }
 
+    public static void printWholeLadder(Point[][] ladder) {
+        StringBuilder sb = new StringBuilder();
+        for (Point[] row : ladder) {
+            printLadderRow(row, sb);
+            sb.append("\n");
+        }
+        System.out.println(new String(sb));
+    }
+
+    private static StringBuilder printLadderRow(Point[] row, StringBuilder sb) {
+        for (Point point : row) {
+            sb.append(point);
+        }
+        return sb;
+    }
 }
