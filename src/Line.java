dd6159cc66a6559923ed6ba960cdc445a0b43bf8
jaea-kim <jaeakim98@gmail.com>
2023-03-08 21:07:00 +0900

    사다리 한 줄을 구성요소 상수로 선언

--- src/Line.java
+++ src/Line.java
@@ -1,4 +1,7 @@
 public class Line {
+    private final char BORDER_LINE = '|';
+    private final char HORIZONTAL_LINE = '-';
+    private final char EMPTY_LINE = ' ';
     private char[] points;
 
     public Line(int lineLength) {
