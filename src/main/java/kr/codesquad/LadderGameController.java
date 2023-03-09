33be0e39e2fb8e9beb2776cca19fd4527a68c935
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-09 16:25:37 +0900

    Refactor: LadderGameController.java 코드 수정
    Converter.java 생성에 따른 LadderGameController 생성자 부분과 ladderGameStart() 메서드 내 코드 수정

--- src/main/java/kr/codesquad/LadderGameController.java
+++ src/main/java/kr/codesquad/LadderGameController.java
@@ -5,18 +5,21 @@ public class LadderGameController {
     private final InputView inputView;
     private final OutputView outputView;
     private final LadderMaker ladderMaker;
+    private final Converter converter;
 
-    LadderGameController(InputView inputView, OutputView outputView, LadderMaker ladderMaker) {
+    LadderGameController(InputView inputView, OutputView outputView, LadderMaker ladderMaker, Converter converter) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.ladderMaker = ladderMaker;
+        this.converter = converter;
     }
 
-    public void LadderGameStart() {
+    public void ladderGameStart() {
         outputView.printAskNameOfPlayers();
         String playersName = inputView.userInputPlayersName();
         outputView.printAskHeightOfLadder();
         int heightOfLadder = inputView.userInputHeightOfLadder();
-        outputView.printLadder(ladderMaker.createLadder(playersName, heightOfLadder));
+        Ladder ladder = ladderMaker.createLadder(playersName, heightOfLadder);
+        outputView.printLadder(converter.convertLadderToString(ladder));
     }
 }
