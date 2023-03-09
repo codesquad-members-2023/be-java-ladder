748ba417a2c0682a7ade8a27350ebe8f703c885d
ngw7617@naver.com <ngw7617@naver.com>
2023-03-09 12:16:20 +0900

    chore:미사용 getter 제거

--- src/main/java/kr/codesquad/domain/Ladder.java
+++ src/main/java/kr/codesquad/domain/Ladder.java
@@ -9,17 +9,12 @@ public class Ladder {
     private List<String> names;
     private ArrayList<Line> ladderMap;
 
-    public List<String> getNameList() {
-        return names;
-    }
-
     public Ladder(List<String> names, ArrayList<Line> ladderMap) {
         this.names = names;
         this.ladderMap = ladderMap;
     }
 
     public String getRenderedString() {
-        //TODO : Render 메서드를 호출하여 String Type으로 리턴하도록 수정
         return Renderer.renderMap(ladderMap, names);
     }
 }
