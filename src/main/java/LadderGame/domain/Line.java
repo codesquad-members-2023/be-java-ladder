0bf9c767008e66540277b63bc2f1a940a9383b83
JeonHyoChang <jhc1426@naver.com>
2023-03-09 12:14:15 +0900

    ♻️refactor: Line 객체 수정(이름, 라인 유무 로직, 생성자 내부 변경)

--- src/main/java/LadderGame/domain/Line.java
+++ src/main/java/LadderGame/domain/Line.java
@@ -5,21 +5,25 @@ import java.util.ArrayList;
 public class Line {
     private final ArrayList<Boolean> points = new ArrayList<>();
 
+    // Line 생성자
     public Line(int countOfPerson) {
-        // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
-        makeLine(countOfPerson);
-    }
-
-    public void makeLine(int countOfPerson) {
         for (int i = 0; i < countOfPerson - 1; i++) {
             boolean before_line = i != 0 && points.get(i - 1);
-            points.add(checkLine(before_line));
+            points.add(makePoint(before_line));
         }
     }
 
     // 사다리 내부 값
-    public boolean makeLine(boolean before_line) {
+    public boolean makePoint(boolean before_line) {
         if ((int) (Math.random() * 2) == 1 && !before_line) {
+            return true;
+        }
+        return false;
+    }
+
+    // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
+    public boolean checkToLine(int countOfPoint) {
+        if (points.get(countOfPoint)) {
             return true;
         }
         return false;
