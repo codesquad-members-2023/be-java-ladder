6b28e932352e9020d9d62c213d24eefddb2f9258
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-09 23:08:41 +0900

    Refactor: InputView.java 코드 수정
    userInputNumberOfPeople() 메서드 미사용에 따른 해당 메서드 주석 처리

--- src/main/java/kr/codesquad/view/InputView.java
+++ src/main/java/kr/codesquad/view/InputView.java
@@ -18,9 +18,9 @@ public class InputView {
         return input;
     }
 
-    public int userInputNumberOfPeople() {
-        return Integer.parseInt(globalReadLine());
-    }
+//    public int userInputNumberOfPeople() {        // 사용하지 않는 메서드
+//        return Integer.parseInt(globalReadLine());
+//    }
 
     public int userInputHeightOfLadder() {
         return Integer.parseInt(globalReadLine());
