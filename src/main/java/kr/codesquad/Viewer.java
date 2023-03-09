8c453c137980a6b0544deb7b522866ef5ebcb45a
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-09 15:22:59 +0900

    refactor: Step-2 피드백 멤버 변수 지역 변수로 대체하기

--- src/main/java/kr/codesquad/Viewer.java
+++ src/main/java/kr/codesquad/Viewer.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class Viewer {
 
-    private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private Header header;
     private LadderMaker ladderMaker;
 
@@ -20,12 +19,16 @@ public class Viewer {
     }
 
     private String[] participantsList() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요");
+
         return br.readLine().split(",");
     }
 
     private String howHigh() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("최대 사다리 높이는 몇 개인가요?");
+
         return br.readLine();
     }
 
