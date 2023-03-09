72005e458b9edc52503f71a801b7f155e015c5d6
birdieHyun <flaehdan@gmail.com>
2023-03-09 14:15:27 +0900

    Refactor : 사용되지 않는 멤버변수와 생성자 삭제, 코드 정렬
    Body : 사용되지 않는 멤버변수와 생성자를 지우고, public 메서드를 위에 배치하고 불필요한 메서드를 정리

--- src/main/java/kr/codesquad/domain/laddergenerator/LadderMaker.java
+++ src/main/java/kr/codesquad/domain/laddergenerator/LadderMaker.java
@@ -5,10 +5,13 @@ import java.util.List;
 import java.util.Random;
 
 public class LadderMaker {
-    private List<List<Boolean>> ladder;
 
-    public LadderMaker() {
-        this.ladder = new ArrayList<>();
+    public List<List<Boolean>> moveNextRow(int peopleNumber, int height) {
+        List<List<Boolean>> ladder = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            ladder.add(moveNextElement(peopleNumber));
+        }
+        return ladder;
     }
 
     private boolean randomGenerateLadder() {
@@ -16,21 +19,11 @@ public class LadderMaker {
         return random.nextBoolean();
     }
 
-
-    public List<List<Boolean>> moveNextRow(int peopleNumber, int height) {
-        for (int i = 0; i < height; i++) {
-            ladder.add(moveNextElement(peopleNumber));
-        }
-        return ladder;
-    }
-
     private List<Boolean> moveNextElement(int peopleNumber) {
         List<Boolean> oneRow = new ArrayList<>();
         oneRow.add(randomGenerateLadder());
         for (int i = 0; i < peopleNumber - 1; i++) {
-
             oneRow.add(addNextElement(oneRow));
-
         }
         return oneRow;
     }
