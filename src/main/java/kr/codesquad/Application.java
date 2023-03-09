73f4a9b52782ed27e1c7c2443c6eea5226aac7af
leegyeongwhan <leekhy02@naver.com>
2023-03-09 21:06:53 +0900

    feat: 메인 로직들이 변경되어 이클래스의 로직들도 변경합니다.
    사다리 헤더와 사다리를 분리했기 때문에 사다리 헤더에 관련된 코드를 추가합니다.

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -1,13 +1,18 @@
 package kr.codesquad;
 
 import java.util.Scanner;
-import static kr.codesquad.OutputView.printLadder;
+
+import static kr.codesquad.OutputView.*;
+
 
 public class Application {
     private final Command command = new Command(new Scanner(System.in));
 
     public void start() {
-        Ladder ladder = new Ladder(command.countMan(), command.countLadder());
-        printLadder(ladder.ladderClone());
+        LadderHeader ladderHeader = new LadderHeader(command.countMembers());
+        CharLadder ladder = new CharLadder(ladderHeader, command.countLadder());
+        ListLadder listLadder = new ListLadder(ladder.charLadderClone(), ladderHeader);
+        printLadderHeader(ladderHeader.getMembersOfLadderHeader());
+        printLadder(listLadder.cloneLadder());
     }
 }
