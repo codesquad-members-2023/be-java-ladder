1e40ad8eb87001466440a296de99ff4038348ffa
kimdongmin <kimdongmin@kimdongminui-MacBookPro.local>
2023-03-09 16:15:26 +0900

    feat(mission3): 사다리 라인 '---' 에서 '-----' 변경

--- src/main/java/kr/codesquad/Shape.java
+++ src/main/java/kr/codesquad/Shape.java
@@ -3,9 +3,7 @@ package kr.codesquad;
 import java.util.Random;
 
 public enum Shape {
-    LADDER_COL_PIECE("|"),
-    LADDER_ROW_PIECE("---"),
-    EMPTY_SPACE("   ");
+    LADDER_COL_PIECE("|"), LADDER_ROW_PIECE("-----"), EMPTY_SPACE("     ");
 
     private String shape;
 
