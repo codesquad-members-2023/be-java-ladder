0477e1ae2998f22852cab1b9eb57563c7d5da0fa
JeonHyoChang <jhc1426@naver.com>
2023-03-09 10:52:47 +0900

    ♻️refactor: 이전 피드백 반영(메소드 명 동사 시작)

--- src/main/java/LadderGame/domain/Line.java
+++ src/main/java/LadderGame/domain/Line.java
@@ -7,18 +7,18 @@ public class Line {
 
     public Line(int countOfPerson) {
         // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
-        setLine(countOfPerson);
+        makeLine(countOfPerson);
     }
 
-    public void setLine(int countOfPerson) {
+    public void makeLine(int countOfPerson) {
         for (int i = 0; i < countOfPerson - 1; i++) {
             boolean before_line = i != 0 && points.get(i - 1);
-            points.add(lineCheck(before_line));
+            points.add(checkLine(before_line));
         }
     }
 
     // 사다리 내부 값
-    public boolean lineCheck(boolean before_line) {
+    public boolean checkLine(boolean before_line) {
         if ((int) (Math.random() * 2) == 1 && !before_line) {
             return true;
         }
