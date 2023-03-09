3d2c360a04d6420fab2a9483351671fbc353b80b
saehim <saeilee.vxv@gmail.com>
2023-03-09 12:16:39 +0900

    Feat (InputView) : 예외 발생시 예외메시지 출력 메서드 구현, 메시지 변수 저장
    - 예외 메시지 변수 저장
    - 예외 발생시 예외 메시지 출력 메서드 구현 (태그 삽입/공백 추가)

--- src/main/java/kr/codesquad/ladder/view/InputView.java
+++ src/main/java/kr/codesquad/ladder/view/InputView.java
@@ -10,6 +10,10 @@ import java.util.stream.Collectors;
 public class InputView {
     private final String GET_PERSON_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요.)";
     private final String GET_STEP_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private final String TAG_EXCEPTION = "[exception] ";
+    private final String NUMBER_FORMAT_ERROR_MESSAGE = "잘못된 숫자 형식입니다.";
+    private final String SMALL_NUMBER_ERROR_MESSAGE = "너무 작은 숫자입니다. (0 초과한 숫자 입력)";
+    private final String BIG_NUMBER_ERROR_MESSAGE = "너무 큰 숫자입니다. (100 이하 숫자 입력)";
     private final String REG = ",";
     private BufferedReader bufferedReader;
 
@@ -39,11 +43,11 @@ public class InputView {
         System.out.println(GET_STEP_MESSAGE);
         int stepNum = getValidInt();
 
-        if (stepNum > 0) {
+        if (stepNum > 0 && stepNum < Integer.MAX_VALUE) {
             return stepNum;
         }
 
-        System.out.println("[exception] 더 큰 숫자를 입력하세요.");
+        printException(SMALL_NUMBER_ERROR_MESSAGE);
         return getStepNum();
     }
 
@@ -53,10 +57,14 @@ public class InputView {
         try {
             result = Integer.parseInt(bufferedReader.readLine());
         } catch (NumberFormatException e) {
-            System.out.println("[exception] 잘못된 숫자 형식입니다.");
+            printException(NUMBER_FORMAT_ERROR_MESSAGE);
             return getValidInt();
         }
 
         return result;
+    }
+
+    private void printException(String message) {
+        System.out.println(TAG_EXCEPTION + message + System.lineSeparator());
     }
 }
