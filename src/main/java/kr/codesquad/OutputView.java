f91653ecb6a15124789140f2de029ff059b85840
leegyeongwhan <leekhy02@naver.com>
2023-03-09 16:54:45 +0900

    feat: printHeader를 출력해주는 메서드추가, 사다리의 자료구조 변경으로 인한 기존 printLadder 변경
    Header를 출력해주는 메서드를 추가 해준다. 기존 사다리의 자료구조가 char[][]에서 2차원 리스트로 변경되었기 때문에
    매개변수와, 메서드 내용을 바꾼다.

--- src/main/java/kr/codesquad/OutputView.java
+++ src/main/java/kr/codesquad/OutputView.java
@@ -1,12 +1,18 @@
 package kr.codesquad;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OutputView {
 
-    public static void printLadder(char[][] ladder) {
-        Arrays.stream(ladder)
-                .map(String::new)
+    public static void printLadderHeader(String members) {
+        System.out.println(members);
+    }
+
+    public static void printLadder(List<ArrayList<String>> ladder) {
+        ladder.stream()
+                .flatMap(List::stream)
                 .forEach(System.out::println);
     }
+
 }
