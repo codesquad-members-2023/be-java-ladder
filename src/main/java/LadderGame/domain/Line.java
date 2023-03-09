0adc312eff4bceb1c92da92421ac238ff2aa4c5d
JeonHyoChang <jhc1426@naver.com>
2023-03-09 09:54:40 +0900

    ✨feat: Line 객체 생성 & Generic 적용 시작

---
+++ src/main/java/LadderGame/domain/Line.java
@@ -0,0 +1,11 @@
+package LadderGame.domain;
+
+import java.util.ArrayList;
+
+public class Line {
+    private ArrayList<Boolean> points = new ArrayList<>();
+
+    public Line(int countOfPerson) {
+        // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
+    }
+}
