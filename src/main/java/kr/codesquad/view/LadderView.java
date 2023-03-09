a20ccce0e3f47d70bf1be962555b01785f127efb
birdieHyun <flaehdan@gmail.com>
2023-03-09 15:16:58 +0900

    Feat : 사람 이름 출력
    Body : 사람 이름 공백 맞춰서 출력하는 기능 추가

--- src/main/java/kr/codesquad/view/LadderView.java
+++ src/main/java/kr/codesquad/view/LadderView.java
@@ -7,7 +7,7 @@ public class LadderView {
     private final String ASK_PEOPLE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private final String ASK_LADDER = "사다리 높이는 몇 개인가요?";
 
-    public void askPeopleNumber(){
+    public void askPeopleNumber() {
         System.out.println(ASK_PEOPLE);
     }
 
@@ -34,5 +34,43 @@ public class LadderView {
 
     public String inputName() {
         return sc.nextLine();
+    }
+
+    public void printNames(String[] nameList){
+        StringBuilder names = new StringBuilder();
+        for (int i = 0; i < nameList.length; i++) {
+            String personName = makeNameLimit(nameList)[i];
+            names.append(personName);
+            names.append(appropriateSpace(personName));
+
+        }
+        System.out.println(names);
+    }
+
+    private String[] makeNameLimit(String[] names) {
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            names[i] = limitNameToFive(name);
+        }
+        return names;
+    }
+
+    private String limitNameToFive(String name) {
+        int maxLength = 5;
+        if (name.length() >= maxLength) {
+            return name.substring(0, 5);
+        }
+        return name;
+    }
+
+    private String appropriateSpace(String name) {
+        StringBuilder spaces = new StringBuilder().append(" ");
+        int maxLength = 5;
+        int space = maxLength - name.length();
+
+        for (int i = 0; i < space; i++) {
+            spaces.append(" ");
+        }
+        return spaces.toString();
     }
 }
