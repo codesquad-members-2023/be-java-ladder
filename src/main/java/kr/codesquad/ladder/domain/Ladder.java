b3070d53a0b0cfcb1bf1fe396ab1a8f828f67836
saehim <saeilee.vxv@gmail.com>
2023-03-09 14:19:57 +0900

    Feat (Ladder) : 유저 수, 라인 수 범위 설정
    - 유저 수, 라인 수 범위 설정
    - 지정된 수 미만이거나 초과하면 예외가 발생한다.

--- src/main/java/kr/codesquad/ladder/domain/Ladder.java
+++ src/main/java/kr/codesquad/ladder/domain/Ladder.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class Ladder {
     private Users users;
     private List<Line> lines;
+    private final int MAX_USER_NUM = 100;
+    private final int MIN_USER_NUM = 1;
+    private final int MAX_LINE_NUM = 100000;
+    private final int MIN_LINE_NUM = 1;
+
 
     public int getNumStep() {
         return lines.size();
@@ -19,8 +24,10 @@ public class Ladder {
         this.lines = generateStep(userList.size(), stepNum);
     }
 
-    private List<Line> generateStep(int userNum, int stepNum) {
-        return LadderMaker.generate(userNum, stepNum);
+    private List<Line> generateStep(int userNum, int lineNum) {
+        isValidLineNum(lineNum);
+        isValidUserNum(userNum);
+        return LadderMaker.generate(userNum, lineNum);
     }
 
     public Line getAStep(int i) {
@@ -29,5 +36,17 @@ public class Ladder {
 
     public String getAUser(int i) {
         return users.getUserName(i);
+    }
+
+    private void isValidUserNum (int userNum) {
+        if (userNum > MAX_USER_NUM || userNum < MIN_USER_NUM) {
+            throw new IllegalArgumentException(String.format("[exception] 참여자 수는 %d~%d 범위 내여야 합니다.", MIN_LINE_NUM, MAX_LINE_NUM));
+        }
+    }
+
+    private void isValidLineNum (int lineNum) {
+        if (lineNum > MAX_LINE_NUM || lineNum <MIN_LINE_NUM) {
+            throw new IllegalArgumentException(String.format("[exception] 사다리 단계는 %d~%d 범위 내여야 합니다.", MIN_LINE_NUM, MAX_LINE_NUM));
+        }
     }
 }
