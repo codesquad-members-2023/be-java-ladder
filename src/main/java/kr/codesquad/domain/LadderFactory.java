47ca69947a9b11ed96e4036ded7c5d909daf2d4f
ngw7617@naver.com <ngw7617@naver.com>
2023-03-09 14:14:29 +0900

    ♻️refactor(Level4):LadderFactory의 Line 초기화 로직을 Line 클래스에서 담당하도록 수정
    - 기존 Factory에서 생성과 관련된 부분을 전부 담당하였는데, 테스트 코드짜기에 어려움이 있어서 Line에서 메서드를 제공하도록 구조를 수정하여 테스트하기 좋은 코드로 변경해보았다.

--- src/main/java/kr/codesquad/domain/LadderFactory.java
+++ src/main/java/kr/codesquad/domain/LadderFactory.java
@@ -14,25 +14,11 @@ public class LadderFactory {
     private static ArrayList<Line> initMap(int numberOfParticipants, int height) {
         ArrayList<Line> ladderMap = new ArrayList<>();
         for (int floorIndex = 0; floorIndex < height; floorIndex++) {
-            ladderMap.add(initLine(numberOfParticipants));
+            ladderMap.add(new Line(numberOfParticipants));
 
         }
         return ladderMap;
     }
 
-    private static Line initLine(int numberOfParticipants) {
-        ArrayList<Boolean> lineComponents = new ArrayList<>();
-        for (int section = 0; section < numberOfParticipants - 1; section++) {
-            initSection(lineComponents, section);
-        }
-        return new Line(lineComponents);
-    }
 
-    private static void initSection(List<Boolean> lineComponents, int section) {
-        if (section > 0 && lineComponents.get(section - 1).equals(Boolean.TRUE)) {
-            lineComponents.add(Boolean.FALSE);
-            return ;
-        }
-        lineComponents.add(RandomGenerator.generate());
-    }
 }
--- src/main/java/kr/codesquad/domain/Line.java
+++ src/main/java/kr/codesquad/domain/Line.java
@@ -2,21 +2,39 @@ package kr.codesquad.domain;
 
 import java.util.ArrayList;
 
-public class Line {
-    private ArrayList<Boolean> sections = new ArrayList<>();
+import kr.codesquad.utils.RandomGenerator;
 
-    public Line(ArrayList<Boolean> sections) {
-        this.sections = sections;
+public class Line {
+    private ArrayList<Boolean> sections;
+
+    public Line(int numberOfParticipants) {
+        this.sections = new ArrayList<>();
+        initLine(numberOfParticipants);
     }
 
-    public Boolean getSection(int section) {
+    public void initLine(int numberOfParticipants) {
+        for (int section = 0; section < numberOfParticipants - 1; section++) {
+            initSection(section);
+        }
+    }
+
+    private void initSection(int section) {
+        //앞 칸에 사다리가 있는 경우 연속으로 사다리를 생성하지 않는다.
+        if (section > 0 && hasConnection(section - 1)) {
+            sections.add(Boolean.FALSE);
+            return ;
+        }
+        sections.add(RandomGenerator.generate());
+    }
+
+    public boolean hasConnection(int section) {
+        return sections.get(section).equals(Boolean.TRUE);
+    }
+
+    public boolean getSection(int section) {
         return sections.get(section);
     }
     public int getSize() {
         return sections.size();
-    }
-
-    public Line (int countOfPerson) {
-        // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
     }
 }
