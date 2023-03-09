1b386a83399ff3be8f0ff0133bc1c1aee3c3115f
CiRe <dltpwns6@naver.com>
2023-03-09 13:49:18 +0900

    refactory : View 클래스의 render 클래스의 2중 for 문을 1중 for 문으로 수정
    사다리를 표현하기 위해 사용되었던 PointMap 자료구조를 2중 for 문을 이용해서, rendering을 진행하였다.
    이것을 가독성을 위해 1중 for 문으로 수정 하였다.

--- src/main/java/kr/codesquad/View.java
+++ src/main/java/kr/codesquad/View.java
@@ -7,25 +7,37 @@ public class View {
         int lineWidth = pointMap[0].length;
         int lineHeight = pointMap.length;
 
-        for (int i = 0; i < lineHeight; i++) {
-            for (int j = 0; j <lineWidth; j++) {
-                switch (pointMap[i][j].getType()) {
-                    case USER -> sb.append("o");
-                    case DESTINATION -> sb.append("x");
-                    case LADDER -> sb.append("|");
-                    case BLOCK -> sb.append(" ");
-                    case LINE -> {
-                        if (pointMap[i][j].getStatus() == Status.CONNECTED) {
-                            sb.append("-");
-                            continue;
-                        }
-                        sb.append(" ");
-                    }
-                }
-            }
-            sb.append("\n");
-        }
+        renderPointMap(pointMap, sb, lineWidth, lineHeight);
 
         System.out.println(sb);
+    }
+
+    private static void renderPointMap(Point[][] pointMap, StringBuilder sb, int lineWidth, int lineHeight) {
+        for (int i = 0; i < lineHeight; i++) {
+            renderRowPoints(pointMap, sb, lineWidth, i);
+            sb.append("\n");
+        }
+    }
+
+    private static void renderRowPoints(Point[][] pointMap, StringBuilder sb, int lineWidth, int positionOfY) {
+        for (int positionOfX = 0; positionOfX < lineWidth; positionOfX++) {
+            renderPoint(pointMap, sb, positionOfY, positionOfX);
+        }
+    }
+
+    private static void renderPoint(Point[][] pointMap, StringBuilder sb, int positionOfY, int positionOfX) {
+        switch (pointMap[positionOfY][positionOfX].getType()) {
+            case USER ->        sb.append("o");
+            case DESTINATION -> sb.append("x");
+            case LADDER ->      sb.append("|");
+            case BLOCK ->       sb.append(" ");
+            case LINE -> {
+                if (pointMap[positionOfY][positionOfX].getStatus() == Status.CONNECTED) {
+                    sb.append("-");
+                    return;
+                }
+                sb.append(" ");
+            }
+        }
     }
 }
