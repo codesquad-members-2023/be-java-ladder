699e0e2c3cc470da18ab5533f900ed0402f661ea
acceptor-gyu <gseonggyu968@gmail.com>
2023-03-09 14:42:17 +0900

    랜덤으로 다리 삽입하는 로직 수정
    기존의 로직은 난수 시퀀스를 만들어 해당 난수 시퀀스를 LadderMaker에게 넘겨주고
    LadderMaker에서 난수 시퀀스를 통해 삽입을 결정했는데
    RandomsGenerator에서 랜덤 boolean을 만들면서 삽입하도록 했음.
    
    이를 통해 필요없는 함수, 변수 등을 삭제할 수 있었다.

--- src/main/java/kr/codesquad/RandomsGenerator.java
+++ src/main/java/kr/codesquad/RandomsGenerator.java
@@ -1,45 +1,35 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class RandomsGenerator {
 
-    /**
-     * generateRandoms 메서드는 사다리 하나의 행마다 다리가 최대
-     * (참여자 수 - 1)만큼 추가될 수 있으므로 (참여자 수 - 1)개의
-     * boolean을 생성해서 반환하는 함수다.
-     *
-     * @param numOfParticipants (참여자 수)
-     * @return 난수 시퀀스
-     */
-    public boolean[] generateRandoms(int numOfParticipants) {
+    private final String BRIDGE = "-----";
+    private final String NONE_BRIDGE = "     ";
 
-        int max = numOfParticipants - 1;
-        boolean[] randoms = new boolean[max];
+    public List<String> generateRandoms(int numOfParticipants) {
+
+        List<String> ladderRow = new ArrayList<>();
+        ladderRow.add("  |");
         Random random = new Random();
 
-        for (int i = 0; i < max; i++) {
-            randoms[i] = random.nextBoolean();
+        boolean prev = random.nextBoolean();
+        for (int i = 0; i < numOfParticipants; i++) {
+            boolean now = random.nextBoolean();
+            ladderRow.add(selectBridge(prev, now));
+            ladderRow.add("|");
+            prev = now;
         }
 
-        return validationContinuous(randoms);
+        return ladderRow;
     }
 
-    private boolean[] validationContinuous(boolean[] randoms) {
-        boolean prev = randoms[0];
-
-        for (int i = 1; i < randoms.length; i++) {
-            continuesBridge(randoms, prev, i);
-            prev = randoms[i];
+    private String selectBridge(boolean prev, boolean now) {
+        if (now && !prev) {
+            return BRIDGE;
         }
-
-        return randoms;
-    }
-
-    private void continuesBridge(boolean[] randoms, boolean prev, int i) {
-        if (prev && randoms[i]) {
-            randoms[i] = false;
-        }
+        return NONE_BRIDGE;
     }
 }
