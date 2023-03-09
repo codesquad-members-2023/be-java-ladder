91801d6b905b96e2663ecf23ab71c4c68bdfe4ad
ngw7617@naver.com <ngw7617@naver.com>
2023-03-09 15:40:39 +0900

    ✨test(Level4):Line클래스 Test 코드 작성
    - random util에서 받아와서 Line 객체에 주입하는 방식으로 테스트 메서드를 구현했다.
    - test 코드를 작성하기 위해 코드를 조금씩 변경하고 있는데 이게 좋은 방향인지는 조금 의문이 남아있다.

--- src/main/java/kr/codesquad/domain/Line.java
+++ src/main/java/kr/codesquad/domain/Line.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import kr.codesquad.utils.RandomGenerator;
-
 public class Line {
     private ArrayList<Boolean> sections;
 
--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class LineTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import kr.codesquad.domain.Line;
+
+class LineTest {
+    @Nested
+    @DisplayName("initLine 메소드는")
+    class initLine {
+        Line line;
+
+        @BeforeEach
+        public void setup() {
+            line = new Line();
+        }
+
+        @Test
+        @DisplayName("라인 객체를 외부에서 전달받은대로 생성하는지 테스트한다.")
+        public void initLineTest() {
+            //Random 주입
+            line.initLine(3, List.of(true, false));
+            assertThat(line.getSections()).isEqualTo(List.of(true, false));
+        }
+
+        @Test
+        @DisplayName("연속된 사다리의 생성이 요구되면 사다리가 연속되지 못하게 생성한다.")
+        public void continuousLineTest() {
+            //Random 주입
+            line.initLine(3, List.of(true, true));
+            assertThat(line.getSections()).isEqualTo(List.of(true, false));
+        }
+    }
+
+    @Nested
+    @DisplayName("hasConnection 메소드는")
+    class hasConnection {
+        @Test
+        @DisplayName("특정 위치가 연결되어있는지 끊어져있는지 확인한다.")
+        public void hasConnectionTest() {
+            Line line = new Line();
+            line.initLine(3, List.of(true, false, true));
+
+            assertThat(line.hasConnection(0)).isEqualTo(true);
+        }
+    }
 }
