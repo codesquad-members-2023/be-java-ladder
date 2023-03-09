97bf76d4668d7f02c5d55daeac6408582642dce4
JeonHyoChang <jhc1426@naver.com>
2023-03-09 10:36:16 +0900

    ♻️refactor: View 분리후 사용하는 로직 수정 & 접근제어자 수정(ALL)

--- src/main/java/LadderGame/Main.java
+++ src/main/java/LadderGame/Main.java
@@ -1,20 +1,19 @@
 package LadderGame;
 
 import LadderGame.domain.Ladder;
+import LadderGame.view.Input;
 
 public class Main {
     public static void main(String[] args) {
-        // 재귀 함수 시작 변수
-        int recursion_num = 0;
-
         // 사다리 값 입력
-        Ladder ladder = new Ladder();
+        Input input = new Input();
+        Ladder ladder = new Ladder(input.inputHeight(), input.inputNames());
 
         // 사다리 설정
-        ladder.setLadder(recursion_num);
+        ladder.setLadder();
 
         // 사다리 출력
         System.out.println();
-        ladder.setPrint(); // 리스트로 변경하면서 재귀방식X
+        ladder.callPrint();
     }
 }
--- src/main/java/LadderGame/domain/Ladder.java
+++ src/main/java/LadderGame/domain/Ladder.java
@@ -1,90 +1,43 @@
 package LadderGame.domain;
 
+import LadderGame.view.Output;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Ladder {
-    private int ladder_height;
-    private List<List<String>> ladder_set;
-    private List<String> names;
+    private final int ladder_height;
+    private final ArrayList<Line> ladder_set;
+    private final ArrayList<String> names;
 
     // 초기값 세팅
-    public Ladder() {
-        input();
+    public Ladder(int ladder_height, String names) {
+        this.ladder_height = ladder_height;
+        this.names = parsingName(names);
         ladder_set = new ArrayList<>();
     }
 
-    // 입력 부분
-    public void input() {
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)\n");
-        this.names = parsingName(in.nextLine());
-
-        System.out.print("최대 사다리 높이는 몇 개인가요?\n");
-        this.ladder_height = in.nextInt();
-    }
-
-    // 이름 분리
-    public List<String> parsingName(String str) {
+    // 이름 분리(5글자 초과는 5자로 컷)
+    public ArrayList<String> parsingName(String str) {
         // map을 통하여 5글자 제한 기능 구현
-        return Arrays.stream(str.split(","))
+        return (ArrayList<String>) Arrays.stream(str.split(","))
                 .map(s -> s.length() > 5 ? s.substring(0, 5) : s)
                 .collect(Collectors.toList());
     }
 
     // 사다리 설정
-    public void setLadder(int line) {
-        List<String> temp_List = new ArrayList<>();
-
-        for (int j = 0; j < names.size() * 2 - 1; j++) {
-            String before_line = j < 2 ? "     " : temp_List.get(j - 2);
-            temp_List.add(lineCheck(j, before_line));
-        }
-        ladder_set.add(temp_List);
-
-        if (line + 1 < ladder_height) {
-            setLadder(line + 1);
+    public void setLadder() {
+        for (int i = 0; i < ladder_height; i++) {
+            ladder_set.add(new Line(names.size()));
         }
     }
 
-    // 이름 출력
-    public void namePrint() {
-        for (String temp : names) {
-            System.out.print(temp);
-            spacePrint(temp.length());
-        }
-        System.out.println();
-    }
+    // 출력 호출
+    public void callPrint() {
+        Output output = new Output();
 
-    // 빈 공간 출력
-    public void spacePrint(int length) {
-        for (int i = 6 - length; i > 0; i--) {
-            System.out.print(" ");
-        }
-    }
-
-    // 사다리 내부 값
-    public String lineCheck(int check, String before_line) {
-        if (check % 2 == 0) {
-            return "|";
-        }
-        if ((int) (Math.random() * 2) == 1 && before_line.equals("     ")) {
-            return "-----";
-        }
-        return "     ";
-    }
-
-    // 사다리 출력
-    public void setPrint() {
-        namePrint();
-
-        for (List<String> temp_List : ladder_set) {
-            temp_List.forEach(System.out::print);
-            System.out.println();
-        }
+        output.namePrint(names);
+        output.setPrint(ladder_set);
     }
 }
