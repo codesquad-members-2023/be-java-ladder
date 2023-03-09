01e11c5def87c512549e78cb1f5aeab5b34960fd
jaea-kim <jaeakim98@gmail.com>
2023-03-09 14:37:12 +0900

    사용하지 않는 함수 삭제

--- src/InputView.java
+++ src/InputView.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int getPlayerNum() {
-        System.out.println("참여할 사람은 몇 명인가요?");
-        return scanner.nextInt();
-    }
-
     public static int getLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
--- src/OutputView.java
+++ src/OutputView.java
@@ -1,10 +1,4 @@
 public class OutputView {
-    public static void printMap(char[][] map) {
-        for (char[] line : map) {
-            printLine(line);
-        }
-    }
-
     private static void printLine(char[] line) {
         for (char c : line) {
             System.out.print(c);
