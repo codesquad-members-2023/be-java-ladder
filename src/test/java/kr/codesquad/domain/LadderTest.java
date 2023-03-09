916e22cf50f23f77397fe5c0904579bf51312069
Hyun <ghkdgus29@naver.com>
2023-03-09 10:39:49 +0900

    fix: LadderTest 컴파일 에러 제거
    Line 클래스 내부에 get 메서드를 제거함으로써, Ladder 클래스는 Line 클래스 내부 자료구조에 대해 알지 못한다.
    따라서 Line 길이에 대한 테스트를 제거

--- src/test/java/kr/codesquad/domain/LadderTest.java
+++ src/test/java/kr/codesquad/domain/LadderTest.java
@@ -11,12 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LadderTest {
 
     @Test
-    @DisplayName("사다리 맵의 가로 세로 크기가 정상적으로 만들어졌는지 테스트")
+    @DisplayName("사다리 맵의 높이가 정상적으로 만들어졌는지 테스트")
     void makeBridge() {
         Ladder ladder = new Ladder();
         List<Line> ladderMap = ladder.make(List.of("pobi", "honux", "crong", "jk"), 5);
 
         assertThat(ladderMap.size()).isEqualTo(5);
-        assertThat(ladderMap.get(0).getPoints().size()).isEqualTo(3);
     }
 }
