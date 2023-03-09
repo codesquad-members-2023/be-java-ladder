78f65a4d2da28e025b7083ce81f28dc3c87b14cd
JeonHyoChang <jhc1426@naver.com>
2023-03-09 09:57:20 +0900

    ♻️refactor: 라인 관련 메소드 Line객체로 옮기기 & 수정

--- src/main/java/LadderGame/domain/Line.java
+++ src/main/java/LadderGame/domain/Line.java
@@ -7,5 +7,22 @@ public class Line {
 
     public Line(int countOfPerson) {
         // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
+        setLine(countOfPerson);
+
+    }
+
+    public void setLine(int countOfPerson) {
+        for (int i = 0; i < countOfPerson - 1; i++) {
+            boolean before_line = i != 0 && points.get(i - 1);
+            points.add(lineCheck(before_line));
+        }
+    }
+
+    // 사다리 내부 값
+    public boolean lineCheck(boolean before_line) {
+        if ((int) (Math.random() * 2) == 1 && !before_line) {
+            return true;
+        }
+        return false;
     }
 }
