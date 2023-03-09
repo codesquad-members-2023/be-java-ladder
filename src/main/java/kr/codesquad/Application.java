315cde7dceac1476d366de5625eb211e7c980e98
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-09 16:26:46 +0900

    Refactor: Application.java 코드 수정
    메서드 호출하는 부분에서 대문자 오탈자 수정

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -4,6 +4,6 @@ public class Application {
     public static void main(String[] args) {
 
         LadderGameController ladderGameController = new LadderGameController(new InputView(), new OutputView(), new LadderMaker(), new Converter());
-        ladderGameController.LadderGameStart();
+        ladderGameController.ladderGameStart();
     }
 }
