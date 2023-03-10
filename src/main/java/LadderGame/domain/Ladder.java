f6cbb3b65d86e123a163d9fa2693f253e49a7f70
JeonHyoChang <jhc1426@naver.com>
2023-03-10 14:29:35 +0900

    ✨feat: 사다리 결과를 도출하기위한 메소드 생성

--- src/main/java/LadderGame/domain/Ladder.java
+++ src/main/java/LadderGame/domain/Ladder.java
@@ -9,20 +9,48 @@ public class Ladder {
     private final int ladderHeight;
     private final ArrayList<Line> ladderSet;
     private final LadderInOutData ladderData;
-    private final HashMap<String, String> resultMap;
 
     // 초기값 세팅
     public Ladder(LadderInOutData ladderData, int ladderHeight) {
         this.ladderHeight = ladderHeight;
         this.ladderData = ladderData;
         ladderSet = new ArrayList<>();
-        resultMap = new HashMap<>();
     }
 
     // 사다리 설정
     public void makeLadder() {
         for (int i = 0; i < ladderHeight; i++) {
             ladderSet.add(new Line(ladderData.getNameSize()));
+        }
+    }
+
+    // 사다리 결과 도출
+    public HashMap<String, String> setResultMap() {
+        HashMap<String, String> resultMap = new HashMap<>();
+
+        for (int i = 0; i < ladderData.getNameSize(); i++) {
+            resultMap.put(ladderData.getName(i)
+                    , ladderData.getResult(parsingResult(i)));
+        }
+
+        return resultMap;
+    }
+
+    // 사람별 결과 도출
+    public int parsingResult(int nameOfNum) {
+        int lineOfNum = nameOfNum;
+
+        for (int i = 0; i < ladderHeight; i++) {
+            lineOfNum +=checkMoveLine(lineOfNum);
+        }
+
+        return lineOfNum;
+    }
+
+    // 사다리 이동 체크
+    public int checkMoveLine(int lineOfNum){
+        if(lineOfNum != ladderData.getNameSize()-1){
+            return
         }
     }
 
