98df34ad2a8ab4da1f7dfac86dd27e1d6d409bca
birdieHyun <flaehdan@gmail.com>
2023-03-10 15:03:27 +0900

    Refactor : 클래스 수정에 따른 의존관계 변경
    LadderMaker 클래스를 Ladder 클래스로 변경함에 따라 변경되는 의존관계 수정

--- src/main/java/kr/codesquad/domain/Controller.java
+++ src/main/java/kr/codesquad/domain/Controller.java
@@ -1,36 +1,31 @@
 package kr.codesquad.domain;
 
 import kr.codesquad.domain.laddergenerator.Ladder;
-import kr.codesquad.domain.laddergenerator.LadderMaker;
 import kr.codesquad.view.LadderView;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
     private final Ladder ladder;
     private final LadderView ladderView;
-    private final LadderMaker ladderMaker;
 
-    public Controller(Ladder ladder, LadderView ladderView, LadderMaker ladderMaker) {
+    public Controller(Ladder ladder, LadderView ladderView) {
         this.ladder = ladder;
         this.ladderView = ladderView;
-        this.ladderMaker = ladderMaker;
 
     }
 
     public void startGame() {
         ladderView.askPeopleNumber();
-        String[] names = peopleName();
+        List<String> names = Arrays.asList(peopleName());
 
-        int peopleNumber = names.length;
 
         ladderView.askLadderHeight();
         int height = ladderView.inputInt();
 
-        List<List<String>> lists = ladder.makeLadder(ladderMaker.makeBooleanLadder(peopleNumber, height));
-
-        ladderView.printNames(names);
-        ladderView.printLadder(lists);
+        String ladderResult = ladder.makeLadder(names, height);
+        ladderView.printLadder(names, ladderResult);
     }
 
     private String[] peopleName() {
