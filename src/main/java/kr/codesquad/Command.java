7395b9ca899fe5159b62ad5b5fc69b18524850b5
leegyeongwhan <leekhy02@naver.com>
2023-03-10 14:14:34 +0900

    feat: 회원당 글자수를 0보다 크고 5만큼 입력 받을 수 있게 처리한다.
    글자를 입력력받고, 글자를 배열로 바꾼후 한 맴버씩 범위에 맞는지 확인한다. 다 통과한다면 headLine 즉입력받은 글자를
    리턴해준다. 아니면 다시 입력 받도록한다.

--- src/main/java/kr/codesquad/Command.java
+++ src/main/java/kr/codesquad/Command.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import static kr.codesquad.Message.*;
 
 public class Command {
+    public static final int NAME_MIN_LENGTH = 1;
+    public static final int NAME_MAX_LENGTH = 6;
     private Scanner scanner;
 
     public Command(Scanner scanner) {
@@ -13,7 +15,34 @@ public class Command {
 
     public String countMembers() {
         System.out.println(START_MESSAGE1);
-        return scanner.nextLine();
+        String headLine = scanner.nextLine();
+        return headLineValidation(headLine);
+    }
+
+    private String headLineValidation(String headLine) {
+        try {
+            validationMembersArray(headLine.split(","));
+            return headLine;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return countMembers();
+        }
+    }
+
+    private void validationMembersArray(String[] members) throws IllegalArgumentException {
+        for (String member : members) {
+            validationMemberName(member);
+        }
+    }
+
+    private void validationMemberName(String name) throws IllegalArgumentException {
+        if (isValidMemberNameLength(name)) {
+            throw new IllegalArgumentException("5글자 넘는다");
+        }
+    }
+
+    private boolean isValidMemberNameLength(String name) {
+        return name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH;
     }
 
     public int countLadder() {
